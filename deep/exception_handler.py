f15576daa8e766c8c5be6c2b5875ada0fbc66e68
thenav56 <ayernavin@gmail.com>
2017-09-21 15:35:03 +0545

    Add errors to logging
    ref : https://github.com/eoglethorpe/deeper/pull/3/files/b3ef3679b85e235c102cfd03647746307de69620#r139957436

--- deep/exception_handler.py
+++ deep/exception_handler.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def custom_exception_handler(exc, context):
@@ -28,6 +31,6 @@ def custom_exception_handler(exc, context):
 
     if settings.DEBUG:
         import traceback
-        print(traceback.format_exc())
+        logger.error(traceback.format_exc())
 
     return response
