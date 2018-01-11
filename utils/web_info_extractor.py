e9f8e0a709620c0e46f6becef55ca3c8b2ab5b5e
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-01-11 18:10:57 +0545

    Create web info extractor
    To extract date, source and project

---
+++ utils/web_info_extractor.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+import articleDateExtractor
+import requests
+import tldextract
+
+from project.models import Project
+
+
+class WebInfoExtractor:
+    def __init__(self, url):
+        self.url = url
+
+        html = requests.get(url).text
+        self.page = BeautifulSoup(html, 'lxml')
+
+    def get_date(self):
+        return articleDateExtractor.extractArticlePublishedDate(
+            self.url
+        )
+
+    def get_country(self):
+        country = self.page.select('.primary-country .country a')
+        if country:
+            return country[0].text.strip()
+
+        country = self.page.select('.country')
+        if country:
+            return country[0].text.strip()
+
+        return None
+
+    def get_project(self):
+        country = self.get_country()
+        if country:
+            return Project.objects.filter(
+                regions__title__icontains=country
+            ).first()
+        return None
+
+    def get_source(self):
+        source = self.page.select('.field-source')
+        if source:
+            return source[0].text.strip()
+
+        return tldextract.extract(self.url).domain
+
+    def get_website(self):
+        return urlparse(self.url).netloc
