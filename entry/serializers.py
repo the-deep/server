9839172875c64f8ffb74a708b4377cf996741f0d
tnagorra <weathermist@gmail.com>
2018-04-27 13:52:01 +0545

    Create request to /edit-entry-data

--- entry/serializers.py
+++ entry/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 
 from deep.serializers import RemoveNullFieldsMixin
 from user_resource.serializers import UserResourceSerializer
-from lead.serializers import SimpleLeadSerializer
+from lead.serializers import LeadSerializer
 from lead.models import Lead
 from analysis_framework.serializers import (
     AnalysisFrameworkSerializer,
     SimpleWidgetSerializer,
 )
-from geo.serializers import GeoOptionSerializer
+from geo.serializers import GeoOptionSerializer, SimpleRegionSerializer
 from .models import (
     Entry, Attribute, FilterData, ExportData
 )
@@ -117,17 +117,20 @@ class EntrySerializer(RemoveNullFieldsMixin,
 
 class EditEntriesDataSerializer(RemoveNullFieldsMixin,
                                 serializers.ModelSerializer):
-    lead = SimpleLeadSerializer(source='*', read_only=True)
+    lead = LeadSerializer(source='*', read_only=True)
     entries = EntrySerializer(source='entry_set', many=True, read_only=True)
     analysis_framework = AnalysisFrameworkSerializer(
         source='project.analysis_framework',
         read_only=True,
     )
     geo_options = serializers.SerializerMethodField()
+    regions = SimpleRegionSerializer(source='project.regions', many=True,
+                                     read_only=True)
 
     class Meta:
         model = Lead
-        fields = ('lead', 'entries', 'analysis_framework', 'geo_options')
+        fields = ('lead', 'entries', 'analysis_framework', 'geo_options',
+                  'regions')
 
     def get_geo_options(self, lead):
         # TODO Check if geo option is required based on analysis framework
