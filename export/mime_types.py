51f10c75aeb4d43b7725b53c1b91c6c97be39012
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-05-20 12:29:03 +0545

    Create `leads` query for all sources
    Tests for connector apis are also now included.

--- export/mime_types.py
+++ export/mime_types.py
@@ -1,2 +1,8 @@
+DOCX_MIME_TYPE = \
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+PDF_MIME_TYPE = \
+    'application/pdf'
 EXCEL_MIME_TYPE = \
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+JSON_MIME_TYPE = \
+    'application/json'
