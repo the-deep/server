b85af4e8c58959c6ad96ebdd0af9e54f04d28814
Navin Ayer <ayernavin@togglecorp.com>
2018-06-29 14:49:04 +0545

    Add Sentry to Celery

--- deep/celery.py
+++ deep/celery.py
@@ -1,5 +1,19 @@
 import os
-from celery import Celery
+import celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
+from django.conf import settings
+
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        if settings.SENTRY_DSN:
+            client = raven.Client(settings.SENTRY_DSN)
+            # register a custom filter to filter out duplicate logs
+            register_logger_signal(client)
+            # hook into the Celery error handler
+            register_signal(client)
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deep.settings')
