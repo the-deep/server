a6925453250a43536d25b41d55bdd2d9eecbac54
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-07-23 18:19:00 +0545

    Fix for filters for geo and organigram widgets

--- entry/widgets/geo_widget.py
+++ entry/widgets/geo_widget.py
@@ -2,7 +2,7 @@ from .utils import set_filter_data, set_export_data
 
 
 def update_attribute(entry, widget, data, widget_data):
-    values = data.get('values', [])
+    values = data.get('value', [])
 
     set_filter_data(
         entry,
