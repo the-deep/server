41ea827db93b4479d1d64f01347a3b51d5d581d9
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-08-22 13:59:21 +0545

    Modify bulk migration to check migrated attribtues

--- bulk_data_migrations/v1_2/organigram.py
+++ bulk_data_migrations/v1_2/organigram.py
@@ -2,8 +2,14 @@ def migrate_widget(widget_data):
     return widget_data
 
 
+def migrate_val(v):
+    if isinstance(v, dict):
+        return v['key']
+    return v
+
+
 def migrate_attribute(data):
     value = data.get('values') or []
     return {
-        'value': [v['id'] for v in value],
+        'value': [migrate_val(v) for v in value],
     }
--- bulk_data_migrations/v1_2/scale.py
+++ bulk_data_migrations/v1_2/scale.py
@@ -1,12 +1,16 @@
 def migrate_widget(widget_data):
     units = widget_data.get('scale_units') or []
     for unit in units:
-        unit['label'] = unit['title']
-        unit.pop('title')
+        if unit.get('title'):
+            unit['label'] = unit['title']
+            unit.pop('title')
     return widget_data
 
 
 def migrate_attribute(data):
+    if data.get('value'):
+        return data
+
     return {
         'value': data.get('selected_scale')
     }
