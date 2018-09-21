1908aa2ead242705f1584ab0399716be57aeab97
Bishal Timilsina <bishaltimilsina@gmail.com>
2018-09-21 17:39:28 +0545

    Thumbnail generation from docs and html

--- utils/extractor/tests/test_file_document.py
+++ utils/extractor/tests/test_file_document.py
@@ -1,4 +1,5 @@
-from os.path import join
+from os.path import (join, isfile)
+import logging
 
 from django.test import TestCase
 from django.conf import settings
@@ -11,6 +12,7 @@ DOCX_FILE = 'doc.docx'
 PPTX_FILE = 'doc.pptx'
 PDF_FILE = 'doc.pdf'
 
+logger = logging.getLogger(__name__)
 
 class FileDocumentTest(TestCase):
     """
@@ -36,20 +38,42 @@ class FileDocumentTest(TestCase):
         # TODO: Verify image
         # self.assertEqual(len(images), 4)
 
+    def thumbnail(self, file_path):
+        ERROR_TYPE = 'THUMBNAIL ERROR'
+        doc = open(join(self.documents, file_path), 'rb')
+
+        try:
+            thumbnail = FileDocument(
+                doc,
+                doc.name.split('/')[-1]
+            ).get_thumbnail()
+        except Exception:
+            import traceback
+            logger.warning('\n' + ('*' * 30))
+            logger.warning('{}: FILEDOCUMENT: {}'.format(ERROR_TYPE, doc.name))
+            logger.warning(traceback.format_exc())
+            return
+
+        self.assertTrue(isfile(thumbnail.name))
+        # TODO: image validation
+
     def test_docx(self):
         """
         Test Docx import
         """
         self.extract(DOCX_FILE)
+        self.thumbnail(DOCX_FILE)
 
     def test_pptx(self):
         """
         Test pptx import
         """
         self.extract(PPTX_FILE)
+        self.thumbnail(PPTX_FILE)
 
     def test_pdf(self):
         """
         Test Pdf import
         """
         self.extract(PDF_FILE)
+        self.thumbnail(PDF_FILE)
--- utils/extractor/tests/test_web_document.py
+++ utils/extractor/tests/test_web_document.py
@@ -51,26 +51,41 @@ class WebDocumentTest(TestCase):
         # TODO: Verify image
         # self.assertEqual(len(images), 4)
 
+    def thumbnail(self, url, file_type):
+        try:
+            thumbnail = WebDocument(url).get_thumbnail()
+        except Exception:
+            import traceback
+            logger.warning('\n' + ('*' * 30))
+            logger.warning('THUMBNAIL ERROR: WEBDOCUMENT: {}'.format(ERROR_TYPE, file_type.upper()))
+            logger.warning(traceback.format_exc())
+            return
+        # TODO: image validation
+
     def test_html(self):
         """
         Test html import
         """
         self.extract(HTML_URL, 'html')
+        self.thumbnail(HTML_URL, 'html')
 
     def test_docx(self):
         """
         Test Docx import
         """
         self.extract(DOCX_URL, 'docx')
+        self.thumbnail(DOCX_URL, 'docx')
 
     def test_pptx(self):
         """
         Test pptx import
         """
         self.extract(PPTX_URL, 'pptx')
+        self.thumbnail(PPTX_URL, 'pptx')
 
     def test_pdf(self):
         """
         Test Pdf import
         """
         self.extract(PDF_URL, 'pdf')
+        self.thumbnail(PDF_URL, 'pdf')
