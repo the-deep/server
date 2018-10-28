e9f0605a41aef097f6c5aabe26a91541bba38c88
bewakes <spirit_bibek@yahoo.com>
2018-10-28 12:16:20 +0545

    Add autodetection of csv and its test case

--- tabular/tasks.py
+++ tabular/tasks.py
@@ -5,7 +5,7 @@ from celery import shared_task
 from redis_store import redis
 from django.db import transaction
 
-from .models import Book
+from .models import Book, Field, Sheet
 from .extractor import csv
 
 
@@ -15,7 +15,59 @@ logger = logging.getLogger(__name__)
 def _tabular_extract_book(book):
     if book.file_type == Book.CSV:
         csv.extract(book)
+    auto_detect_and_update_fields(book)
     return True
+
+
+def auto_detect_and_update_fields(book):
+    for sheet in book.sheet_set.all():
+        data = sheet.data
+
+        field_ids = data[0].keys() if data else []
+        field_ids = [x for x in field_ids if x.isnumeric()]  # omit non numeric
+        fields = Field.objects.filter(id__in=field_ids)
+
+        fields_default_types = {x.id: x.type for x in fields}
+        fields_current_types = {x.id: None for x in fields}
+        fields_set_default = {x.id: False for x in fields}
+
+        for row in data:
+            for k, v in row.items():
+                type = Field.STRING
+                if parse_number(v):
+                    type = Field.NUMBER
+                # elif parse_date(v):
+                #     type = Field.DATE
+                else:
+                    type = Field.STRING
+
+                curr_type = fields_current_types.get(k)
+
+                if not k.isnumeric():  # because k is the pk of field
+                    continue
+                key = int(k)
+                # no need to check further if the field has default type
+                if fields_set_default.get(key) is True:
+                    continue
+                if curr_type is not None and curr_type != type:
+                    # mixed types present, set original type
+                    fields_set_default[key] = True
+                    fields_current_types[key] = fields_default_types.get(k) \
+                        or Field.STRING
+                elif curr_type is None:
+                    fields_current_types[key] = type
+
+        for field in fields:
+            field.type = fields_current_types.get(field.id, Field.STRING)
+            field.save()
+
+
+def parse_number(val):
+    try:
+        float(val)
+        return True
+    except ValueError as ve:
+        return False
 
 
 @shared_task
--- tabular/tests.py
+++ tabular/tests.py
@@ -1,3 +1,98 @@
-from django.test import TestCase
+import os
+from rest_framework.test import APITestCase
+from autofixture.base import AutoFixture
+from tempfile import NamedTemporaryFile
 
-# Create your tests here.
+from django.conf import settings
+
+from gallery.models import File
+
+from .tasks import auto_detect_and_update_fields
+from .extractor import csv
+from .models import Book, Field
+
+consistent_csv_data = '''id,age,address\n1,10,chitwan\n2,30,kathmandu'''
+inconsistent_csv_data = '''id,age,address\n1,10,chitwan\nabc,30,kathmandu'''
+
+
+class TestTabularExtraction(APITestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.files = []
+
+    def setUp(self):
+        Book.objects.all().delete()
+
+    def test_auto_detection_consistent(self):
+        """
+        Testing rows having consistent numeric values
+        """
+        book = self.initialize_data_and_basic_test(consistent_csv_data)
+        # now auto detect
+        auto_detect_and_update_fields(book)
+
+        # now validate auto detected fields
+        for field in Field.objects.all():
+            if field.title == 'id':
+                assert field.type == Field.NUMBER, 'id is number'
+            elif field.title == 'age':
+                assert field.type == Field.NUMBER, 'age is number'
+            elif field.title == 'address':
+                assert field.type == Field.STRING, 'address is string'
+
+    def test_auto_detection_inconsistent(self):
+        """
+        Testing rows having inconsistent numeric values(for field id)
+        """
+        book = self.initialize_data_and_basic_test(inconsistent_csv_data)
+        # now auto detect
+        auto_detect_and_update_fields(book)
+
+        # now validate auto detected fields
+        for field in Field.objects.all():
+            if field.title == 'id':
+                assert field.type == Field.STRING,\
+                    'id is string as it is inconsistent'
+            elif field.title == 'age':
+                assert field.type == Field.NUMBER, 'age is number'
+            elif field.title == 'address':
+                assert field.type == Field.STRING, 'address is string'
+
+    def initialize_data_and_basic_test(self, csv_data):
+        file = NamedTemporaryFile('w', dir=settings.MEDIA_ROOT, delete=False)
+
+        self.files.append(file.name)
+
+        for x in csv_data.split():
+            file.write('{}\n'.format(x))
+        file.close()
+        # create a book
+        csvfile = AutoFixture(
+            File,
+            field_values={
+                'file': file.name
+            }
+        ).create_one()
+        book = AutoFixture(
+            Book,
+            field_values={
+                'file': csvfile
+            }
+        ).create_one()
+        csv.extract(book)
+        assert Field.objects.count() == 3
+        fieldnames = {}
+        for field in Field.objects.all():
+            fieldnames[field.title] = True
+            assert field.type == Field.STRING, "Initial type is string"
+        assert 'id' in fieldnames, 'id should be a fieldname'
+        assert 'age' in fieldnames, 'age should be a fieldname'
+        assert 'address' in fieldnames, 'address should be a field name'
+
+        return book
+
+    def tearDown(self):
+        """Remove temp files"""
+        for file in self.files:
+            os.remove(file)
