25aa9dca27aa21538fbbc75169b2f524d7e7e00a
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-11-20 16:11:58 +0545

    Add `regions` and `admin_levels` in geodata serializer

--- tabular/serializers.py
+++ tabular/serializers.py
@@ -8,16 +8,40 @@ from deep.serializers import (
     NestedUpdateMixin,
 )
 
-from .tasks import tabular_meta_extract_book
 from deep.serializers import RemoveNullFieldsMixin
 from user_resource.serializers import UserResourceSerializer
+
+from geo.serializers import SimpleRegionSerializer, Region, AdminLevel
+
 from .models import Book, Sheet, Field, Geodata
+from .tasks import tabular_meta_extract_book
 
 
 class GeodataSerializer(RemoveNullFieldsMixin, serializers.ModelSerializer):
+    regions = serializers.SerializerMethodField()
+    admin_levels = serializers.SerializerMethodField()
+
     class Meta:
         model = Geodata
         exclude = ('field',)
+
+    def get_regions(self, geodata):
+        if not geodata.data:
+            return []
+        area_ids = [d['selected_id'] for d in geodata.data]
+        regions = Region.objects.filter(
+            adminlevel__geoarea__id__in=area_ids
+        ).distinct()
+        return SimpleRegionSerializer(regions, many=True).data
+
+    def get_admin_levels(self, geodata):
+        if not geodata.data:
+            return []
+        area_ids = [d['selected_id'] for d in geodata.data]
+        admin_levels = AdminLevel.objects.filter(
+            geoarea__id__in=area_ids
+        ).distinct()
+        return admin_levels.values_list('id', flat=True)
 
 
 class FieldSerializer(RemoveNullFieldsMixin, serializers.ModelSerializer):
