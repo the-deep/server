e2bf17d9027bf77cb5b4b46048760e0bda9e3d5b
Aditya Khatri <aditya.khatri@togglecorp.com>
2018-11-23 12:13:36 +0545

    Fix usergroup role migration

--- project/migrations/0046_projectusergroupmembership_role.py
+++ project/migrations/0046_projectusergroupmembership_role.py
@@ -17,6 +17,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='projectusergroupmembership',
             name='role',
-            field=models.ForeignKey(default=project.models.get_default_role_id, on_delete=django.db.models.deletion.CASCADE, to='project.ProjectMembership'),
+            field=models.ForeignKey(default=project.models.get_default_role_id, on_delete=django.db.models.deletion.CASCADE, to='project.ProjectRole'),
         ),
     ]
