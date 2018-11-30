e173f7017024a4b010349c537ba8e232259468ab
frozenhelium <fren.ankit@gmail.com>
2018-11-30 16:42:06 +0545

    Add unseen notification count api

--- notification/receivers.py
+++ notification/receivers.py
@@ -7,14 +7,38 @@ from project.models import ProjectJoinRequest
 
 @receiver(post_save, sender=ProjectJoinRequest)
 def create_notification(sender, instance, created, **kwargs):
-    if (created):
-        admins = instance.project.get_admins()
+    admins = instance.project.get_admins()
 
+    if (created):
         for admin in admins:
+            data = {'join_request_id': instance.id}
             Notification.objects.create(
                 receiver=admin,
-                notification_type='project_join',
+                notification_type=Notification.PROJECT_JOIN_REQUEST,
                 project=instance.project,
-                status='unseen',
+                data=data,
             )
 
+        return
+
+    # Changing unseen to seen for obsolete action (Not implemented)
+    """
+    notifications = Notification.objects.filter(
+        data__id=instance.id,
+        status='unseen',
+    )
+
+    for notification in notifications:
+        notification.status = 'seen'
+        notification.save()
+    """
+
+    for admin in admins:
+        data = {'join_request_id': instance.id}
+
+        Notification.objects.create(
+            receiver=admin,
+            notification_type=Notification.PROJECT_JOIN_RESPONSE,
+            project=instance.project,
+            data=data,
+        )
--- notification/views.py
+++ notification/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.utils.dateparse import parse_datetime
 from .serializers import (NotificationSerializer)
 from .models import (Notification)
 
+from rest_framework.decorators import list_route
+
 from rest_framework import (
-    permissions,
     response,
+    permissions,
     viewsets,
 )
 
@@ -22,9 +24,21 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
         qs = super().filter_queryset(queryset)
         project = self.request.query_params.get('project')
 
-        # TODO: implement ordering
-
         if project is not None:
             qs.filter(project=project)
 
         return qs
+
+    @list_route(
+        permission_classes=[permissions.IsAuthenticated],
+        url_path='unseen-count',
+    )
+    def get_count(self, request, version=None):
+        qs = self.filter_queryset(self.get_queryset())
+        total = qs.count()
+
+        unseen_count = qs.filter(
+            status=Notification.STATUS_UNSEEN).count()
+
+        result = {'unseen': unseen_count, 'total': total}
+        return response.Response(result)
