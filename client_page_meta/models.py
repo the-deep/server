0ea2ef8eecd9130ebde736f44f9c8b44d8ef9bbc
Navin Ayer <ayernavin@togglecorp.com>
2018-12-02 12:06:23 +0545

    Make help_url not empty

--- client_page_meta/models.py
+++ client_page_meta/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=300)
     page_id = models.CharField(max_length=300, unique=True)
-    help_url = models.TextField(blank=True)
+    help_url = models.TextField()
 
     def __str__(self):
         return '{} {}'.format(self.title, self.page_id)
