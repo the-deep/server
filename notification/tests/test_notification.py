7c05c8bffbeabb3beb0c0774eddbbe89285d7d4b
bewakes <spirit_bibek@yahoo.com>
2018-12-03 12:52:42 +0545

    Add tests for join request cancel notification

---
+++ notification/tests/test_notification.py
@@ -0,0 +1,131 @@
+from deep.tests import TestCase
+
+from user.models import User
+from notification.models import Notification
+from project.models import ProjectJoinRequest, Project
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class TestNotification(TestCase):
+    """Unit test for Notification"""
+    def setUp(self):
+        super().setUp()
+        # Clear all notifications
+        Notification.objects.all().delete()
+
+    def test_notification_created_on_project_join_request(self):
+        project = self.create(Project, role=self.admin_role)
+        # Create users
+        normal_user = self.create(User)
+        admin_user = self.create(User)
+
+        # Add admin user to project
+        project.add_member(admin_user, role=self.admin_role)
+        ProjectJoinRequest.objects.create(
+            project=project,
+            requested_by=normal_user,
+            role=self.normal_role
+        )
+        # Get notifications for admin_users
+        for user in [self.user, admin_user]:
+            notifications = Notification.get_for(user)
+            assert notifications.count() == 1,\
+                "A notification should have been created for admin"
+
+            notification = notifications[0]
+            assert notification.status == Notification.STATUS_UNSEEN
+            assert notification.notification_type ==\
+                Notification.PROJECT_JOIN_REQUEST
+            assert notification.receiver == user
+            assert notification.data['status'] == 'pending'
+
+    def test_notification_updated_on_request_accepted(self):
+        project = self.create(Project, role=self.admin_role)
+        # Create users
+        normal_user = self.create(User)
+
+        join_request = ProjectJoinRequest.objects.create(
+            project=project,
+            requested_by=normal_user,
+            role=self.normal_role
+        )
+
+        # Get notification for self.user
+        notifications = Notification.get_for(self.user)
+        assert notifications.count() == 1
+        assert notifications[0].notification_type ==\
+            Notification.PROJECT_JOIN_REQUEST
+
+        # Update join_request by adding member
+        project.add_member(join_request.requested_by, role=join_request.role)
+
+        # Manually updateing join_request because add_member does not trigger
+        # receiver for join_request post_save
+        join_request.status = 'accepted'
+        join_request.role = join_request.role
+        join_request.save()
+
+        # Get notifications
+        notifications = Notification.get_for(self.user)
+        assert notifications.count() == 2
+        assert notifications.filter(
+            notification_type=Notification.PROJECT_JOIN_RESPONSE
+        ).count() == 1
+
+    def test_notification_updated_on_request_rejected(self):
+        project = self.create(Project, role=self.admin_role)
+        # Create users
+        normal_user = self.create(User)
+
+        join_request = ProjectJoinRequest.objects.create(
+            project=project,
+            requested_by=normal_user,
+            role=self.normal_role
+        )
+
+        # Get notification for self.user
+        notifications = Notification.get_for(self.user)
+        assert notifications.count() == 1
+        assert notifications[0].notification_type ==\
+            Notification.PROJECT_JOIN_REQUEST
+        assert notifications[0].data['status'] == 'pending'
+
+        # Update join_request without adding member
+        join_request.status = 'rejected'
+        join_request.role = join_request.role
+        join_request.save()
+
+        # Get notifications
+        notifications = Notification.get_for(self.user)
+        assert notifications.count() == 2
+        assert notifications.filter(
+            notification_type=Notification.PROJECT_JOIN_RESPONSE
+        ).count() == 1
+
+    def test_notification_updated_on_request_aborted(self):
+        project = self.create(Project, role=self.admin_role)
+        normal_user = self.create(User)
+
+        join_request = ProjectJoinRequest.objects.create(
+            project=project,
+            requested_by=normal_user,
+            role=self.normal_role
+        )
+
+        # Get notification for self.user
+        notifications = Notification.get_for(self.user)
+        assert notifications.count() == 1
+        assert notifications[0].notification_type ==\
+            Notification.PROJECT_JOIN_REQUEST
+        assert notifications[0].data['status'] == 'pending'
+
+        # Now abort join request by deleting it
+        join_request.delete()
+
+        # Get notifications again
+        notifications = Notification.get_for(self.user)
+        assert notifications.count() == 2
+        new_notif = notifications.order_by('timestamp')[0]
+        assert new_notif.data['status'] == 'aborted'
