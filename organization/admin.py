0ce1f3c3fbb1075623efacf25c35079741e1319b
Navin Ayer <ayernavin@togglecorp.com>
2018-12-30 10:26:02 +0545

    Add search and filter to Organization in admin panel

--- organization/admin.py
+++ organization/admin.py
@@ -19,3 +19,6 @@ def reset_donor(modeladmin, request, queryset):
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     actions = [set_donor, reset_donor]
+    search_fields = ('title', 'short_name', 'long_name')
+    list_display = ('title', 'short_name', 'donor', 'organization_type',)
+    list_filter = ('donor', 'organization_type',)
