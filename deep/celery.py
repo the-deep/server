71bf45d5d1f9672881ecef06f957af675348b195
thenav56 <ayernavin@togglecorp.com>
2019-05-09 16:51:27 +0545

    Using new sentry sdk

--- deep/celery.py
+++ deep/celery.py
@@ -1,19 +1,21 @@
 import os
 import sys
 import celery
-import raven
-from raven.contrib.celery import register_signal, register_logger_signal
+from sentry_sdk.integrations.celery import CeleryIntegration
+
 from django.conf import settings
+
+from utils import sentry
 
 
 class Celery(celery.Celery):
     def on_configure(self):
         if settings.SENTRY_DSN:
-            client = raven.Client(**settings.RAVEN_CONFIG)
-            # register a custom filter to filter out duplicate logs
-            register_logger_signal(client)
-            # hook into the Celery error handler
-            register_signal(client)
+            sentry.init_sentry(
+                app_type='WORKER',
+                integrations=[CeleryIntegration()],
+                **settings.SENTRY_CONFIG
+            )
 
 
 # set the default Django settings module for the 'celery' program.
