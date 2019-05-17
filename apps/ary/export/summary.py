1e134ab024914ba4d3d558f4fc6269a6ebc1993a
bewakes <spirit_bibek@yahoo.com>
2019-05-17 20:05:43 +0545

    Add affected and focus in summary sheet

--- apps/ary/export/summary.py
+++ apps/ary/export/summary.py
@@ -1,5 +1,10 @@
 from utils.common import combine_dicts
-from ary.models import MethodologyField
+from ary.models import (
+    MethodologyField,
+    Focus,
+    Sector,
+    AffectedGroup,
+)
 
 
 def get_assessment_export_summary(assessment):
@@ -23,9 +28,25 @@ def get_assessment_export_summary(assessment):
             for x in all_collection_fields[0].options.all()
         }
 
-    methodology = assessment.get_metadata_json()
-    attrs = combine_dicts(methodology['Attributes'])
+    metadata = assessment.get_metadata_json()
+    attrs = combine_dicts(metadata['Attributes'])
     collection_techniques = combine_dicts(attrs['Collection Technique'])
+
+    methodology = assessment.get_methodology_json()
+
+    focuses = [x.title for x in Focus.objects.filter(template=template)]
+    selected_focuses = set(methodology['Focuses'])
+
+    sectors = [x.title for x in Sector.objects.filter(template=template)]
+    selected_sectors = set(methodology['Sectors'])
+
+    root_affected_group = AffectedGroup.objects.filter(template=template, parent=None)
+    all_affected_groups = root_affected_group.get_children_list() if root_affected_group else []
+
+    selected_affected_groups_ids = {x['key'] for x in methodology['Affected Groups']}
+
+    locations = assessment.methodology['locations']
+    # TODO: Get regions and admin levels
 
     data = {
         'stakeholders': {},  # TODO
@@ -41,10 +62,25 @@ def get_assessment_export_summary(assessment):
             **all_collection_techniques,
             **{x: 1 for x in collection_techniques}
         },
-        'focuces': {},
-        'sectors': {},
-        'affected_groups': {},
-        'location': {},
-        'methodology_content': {},
+        'focuses': {
+            x: 1 if x in selected_focuses else 0
+            for x in focuses
+        },
+        'sectors': {
+            x: 1 if x in selected_sectors else 0
+            for x in sectors
+        },
+        'affected_groups': {
+            x: 1 if x['id'] in selected_affected_groups_ids else 0
+            for x in all_affected_groups
+        },
+        'location': {
+        },
+        'methodology_content': {
+            'objectives': 1 if methodology['Objectives'] else 0,
+            'data_collection_techniques': 1 if methodology['Data Collection Techniques'] else 0,
+            'sampling': 1 if methodology['Sampling'] else 0,
+            'limitations': 1 if methodology['Limitations'] else 0,
+        },
     }
     return data
