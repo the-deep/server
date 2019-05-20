cf797c9069a6959c3d2a69e51449504dcb651f98
bewakes <spirit_bibek@yahoo.com>
2019-05-20 12:20:39 +0545

    Json for summary and affected_groups

--- apps/ary/export/summary.py
+++ apps/ary/export/summary.py
@@ -1,6 +1,10 @@
 from utils.common import combine_dicts
+
+from geo.models import GeoArea
+
 from ary.models import (
     MethodologyField,
+    MetadataField,
     Focus,
     Sector,
     AffectedGroup,
@@ -29,10 +33,10 @@ def get_assessment_export_summary(assessment):
         }
 
     metadata = assessment.get_metadata_json()
-    attrs = combine_dicts(metadata['Attributes'])
-    collection_techniques = combine_dicts(attrs['Collection Technique'])
-
     methodology = assessment.get_methodology_json()
+
+    attrs = combine_dicts(methodology['Attributes'])
+    collection_techniques = combine_dicts(attrs['Collection Technique'])
 
     focuses = [x.title for x in Focus.objects.filter(template=template)]
     selected_focuses = set(methodology['Focuses'])
@@ -40,16 +44,42 @@ def get_assessment_export_summary(assessment):
     sectors = [x.title for x in Sector.objects.filter(template=template)]
     selected_sectors = set(methodology['Sectors'])
 
-    root_affected_group = AffectedGroup.objects.filter(template=template, parent=None)
+    root_affected_group = AffectedGroup.objects.filter(template=template, parent=None).first()
     all_affected_groups = root_affected_group.get_children_list() if root_affected_group else []
+
+    # All affected groups does not have title, so generate title from parents
+    processed_affected_groups = [
+        {
+            'title': ' and'.join(x['parents'][::-1]),
+            'id': x['id']
+        } for x in all_affected_groups
+    ]
 
     selected_affected_groups_ids = {x['key'] for x in methodology['Affected Groups']}
 
     locations = assessment.methodology['locations']
-    # TODO: Get regions and admin levels
+
+    geo_areas = GeoArea.objects.filter(id__in=locations).prefetch_related('admin_level')
+    admin_levels = {f'Admin {x+1}': 0 for x in range(6)}
+
+    for geo in geo_areas:
+        level = geo.admin_level.level
+        key = f'Admin {level}'
+        admin_levels[key] = admin_levels.get(key, 0) + 1
+
+    stakeholders = metadata['Stakeholders']
+    lead_org = stakeholders[0]
+    other_orgs = [x for x in stakeholders[1:]]
 
     data = {
-        'stakeholders': {},  # TODO
+        'stakeholders': {
+            lead_org['schema']['name']: lead_org['value'][0]['name'],
+            **{
+                x['schema']['name']: len(x['value'])
+                if x['schema']['type'] == MetadataField.MULTISELECT else 1
+                for x in other_orgs
+            }
+        },
 
         'additional_documents': {
             'Executive Summary': 1 if additional_documents['executive_summary'] else 0,
@@ -71,11 +101,10 @@ def get_assessment_export_summary(assessment):
             for x in sectors
         },
         'affected_groups': {
-            x: 1 if x['id'] in selected_affected_groups_ids else 0
-            for x in all_affected_groups
+            x['title']: 1 if x['id'] in selected_affected_groups_ids else 0
+            for x in processed_affected_groups
         },
-        'location': {
-        },
+        'location': admin_levels,
         'methodology_content': {
             'objectives': 1 if methodology['Objectives'] else 0,
             'data_collection_techniques': 1 if methodology['Data Collection Techniques'] else 0,
