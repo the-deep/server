91e1e3b4315aeec7b01b1c0a6524aa107eeaeb90
bewakes <spirit_bibek@yahoo.com>
2019-05-27 14:19:08 +0545

    Check cells based on geo title and admin level

--- apps/tabular/receivers.py
+++ apps/tabular/receivers.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.dispatch import receiver
 
 from tabular.models import Field
-from tabular.utils import get_geos_dict
+from tabular.utils import get_geos_dict, get_geos_codes_from_geos_names
 
 
 @receiver(models.signals.pre_save, sender=Field)
@@ -22,8 +22,7 @@ def on_field_saved(sender, **kwargs):
     geos_names = geos_codes = {}
     if field.type == Field.GEO:
         geos_names = get_geos_dict(field.sheet.book.project)
-        geos_codes = {v['code'].lower(): v for k, v in geos_names.items()}
-
+        geos_codes = get_geos_codes_from_geos_names(geos_names)
     cast_info = field.cast_data(geos_names, geos_codes)
 
     field.data = cast_info['values']
