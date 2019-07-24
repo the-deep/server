2ec54ea620840be89edb2425fb007d928e81d48e
bewakes <spirit_bibek@yahoo.com>
2019-07-24 15:45:31 +0545

    Update framework cannot be edited by the created user

--- apps/analysis_framework/models.py
+++ apps/analysis_framework/models.py
@@ -64,8 +64,6 @@ class AnalysisFramework(UserResource):
         * the framework belongs to a project where the user is admin
         """
         return (
-            self.created_by == user or
-            user.is_superuser or
             AnalysisFrameworkMembership.objects.filter(
                 member=user,
                 framework=self,
