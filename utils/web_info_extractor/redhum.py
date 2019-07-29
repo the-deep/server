f3c17e1b9243e540400d65bacabba582382b89fb
thenav56 <ayernavin@togglecorp.com>
2019-07-29 14:09:25 +0545

    Add Redhum custom web info extractor

---
+++ utils/web_info_extractor/redhum.py
@@ -0,0 +1,44 @@
+import re
+from urllib.parse import urlparse
+import requests
+
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36', # noqa
+}
+
+
+class RedhumWebInfoExtractor:
+    def __init__(self, url):
+        self.url = url
+        self.page = {}
+        url_parse = re.search(r'https://redhum.org/documento/(?P<report_id>\d+)\/?$', url)
+        if not url_parse:
+            return
+        report_id = url_parse.group('report_id')
+        rw_url = f'https://api.reliefweb.int/v1/reports/{report_id}'
+        params = {
+            'appname': 'redhum',
+            'fields[include][]': ['title', 'primary_country', 'source', 'date'],
+        }
+
+        try:
+            response = requests.get(rw_url, headers=HEADERS, params=params)
+            self.page = response.json()['data'][0]['fields']
+        except Exception:
+            return
+
+    def get_title(self):
+        return self.page.get('title')
+
+    def get_date(self):
+        return self.page.get('date', {}).get('created', '').split('T')[0]
+
+    def get_country(self):
+        return self.page.get('primary_country', {}).get('name')
+
+    def get_source(self):
+        return self.page.get('source', [{}])[0].get('longname')
+
+    def get_website(self):
+        return urlparse(self.url).netloc
