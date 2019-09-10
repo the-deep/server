16fd4b3b231981e31b520d5fd39246199af6f9d4
bewakes <spirit_bibek@yahoo.com>
2019-09-10 14:03:43 +0545

    Fix emm connector

--- apps/connector/admin.py
+++ apps/connector/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 
 from deep.admin import VersionAdmin
-from connector.models import Connector, EMMEntity
+from connector.models import Connector
 
 
 @admin.register(Connector)
 class ConnectorAdmin(VersionAdmin):
     autocomplete_fields = ('created_by', 'modified_by',)
-
-
-@admin.register(EMMEntity)
-class EMMEntityAdmin(admin.ModelAdmin):
-    pass
