6a369d79f8017f4b02150f0d77828b5c8330b298
bewakes <spirit_bibek@yahoo.com>
2019-09-15 11:22:44 +0545

    Initial setup

--- deep/__init__.py
+++ deep/__init__.py
@@ -1,5 +1,11 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
+import importlib.util
 
 __all__ = ['celery_app']
+
+
+mypy_package = importlib.util.find_spec("mypy")
+if mypy_package:
+    from .checks import mypy  # noqa
