36133ec891f4e4cdbef7bba223aff240b929f689
thenav56 <ayernavin@togglecorp.com>
2020-01-14 16:09:14 +0545

    Fix entry not saving issue

--- deep/serializers.py
+++ deep/serializers.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from rest_framework import serializers
 
 from .writable_nested_serializers import ( # noqa F401
+    ListToDictField,
     NestedCreateMixin,
     NestedUpdateMixin,
     UniqueFieldsMixin,
@@ -40,45 +41,6 @@ class RemoveNullFieldsMixin:
                 if field in data and not data.get(field):
                     data[field] = ''
         return super().to_internal_value(data)
-
-
-class ListToDictField(serializers.Field):
-    """
-    Represent a list of entities as a dictionary
-    """
-    def __init__(self, *args, **kwargs):
-        self.child = kwargs.pop('child')
-        self.key = kwargs.pop('key')
-
-        assert self.child.source is None, (
-            'The `source` argument is not meaningful when '
-            'applied to a `child=` field. '
-            'Remove `source=` from the field declaration.'
-        )
-
-        super().__init__(*args, **kwargs)
-
-    def to_representation(self, obj):
-        list_data = self.child.to_representation(obj)
-        data = {}
-
-        for item in list_data:
-            key_value = item.pop(self.key)
-            data[str(key_value)] = item
-        return data
-
-    def to_internal_value(self, data):
-        list_data = self.to_list_data(data)
-        return self.child.run_validation(list_data)
-
-    def to_list_data(self, data):
-        list_data = []
-        for key, value in data.items():
-            list_data.append({
-                self.key: key,
-                **value,
-            })
-        return list_data
 
 
 class RecursiveSerializer(serializers.Serializer):
