7f76474ddb800a746126502a95ff13a82968ba09
thenav56 <ayernavin@togglecorp.com>
2020-01-23 17:31:17 +0545

    Try parallel test run

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,13 +4,15 @@ export PYTHONUNBUFFERED=1
 
 if [ "$CI" == "true" ]; then
     pip3 install -r requirements.txt
-    pip3 install codecov
+    pip3 install codecov pytest-xdist
     python3 manage.py migrate --no-input
     python3 manage.py createinitialrevisions
     python3 manage.py run_celery_dev &
 
     set -e
-    coverage run -m py.test
+    echo 'import coverage; coverage.process_startup()' > /code/sitecustomize.py
+    COVERAGE_PROCESS_START=`pwd`/.coveragerc COVERAGE_FILE=`pwd`/.coverage PYTHONPATH=`pwd` py.test -n auto --dist=loadfile
+    coverage combine
     coverage report -i
     coverage html -i
     codecov
