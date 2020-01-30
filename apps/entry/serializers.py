ec6818eac8e555e45ccee8a160c50a7adb9ccdbb
thenav56 <ayernavin@togglecorp.com>
2020-01-30 18:51:40 +0545

    Add Project label stat in entry view API

--- apps/entry/serializers.py
+++ apps/entry/serializers.py
@@ -2,6 +2,7 @@ import logging
 
 from drf_dynamic_fields import DynamicFieldsMixin
 from rest_framework import serializers
+from django.db import models
 
 from deep.serializers import (
     RemoveNullFieldsMixin,
@@ -189,20 +190,29 @@ class EntrySerializer(RemoveNullFieldsMixin,
         model = Entry
         fields = '__all__'
 
-    def get_project_labels(self):
-        pass
+    def get_project_labels(self, entry):
+        label_count = self.context.get('entry_group_label_count')
+        if label_count:  # NOTE: Provided by EntryFilterView
+            return label_count.get(entry.pk) or []
+        # Fallback (NOTE: Also update in view context)
+        return entry.entrygrouplabel_set.order_by().values('label').annotate(
+            count=models.Count('id')
+        ).values(
+            'count',
+            label_id=models.F('label__id'),
+            label_color=models.F('label__color'),
+            label_title=models.F('label__title')
+        )
 
     def get_resolved_comment_count(self, entry):
-        return getattr(
-            entry, 'resolved_comment_count',
-            entry.entrycomment_set.filter(parent=None, is_resolved=True).count()
-        )
+        if hasattr(entry, 'resolved_comment_count'):
+            return entry.resolved_comment_count
+        return entry.entrycomment_set.filter(parent=None, is_resolved=True).count()
 
     def get_unresolved_comment_count(self, entry):
-        return getattr(
-            entry, 'unresolved_comment_count',
-            entry.entrycomment_set.filter(parent=None, is_resolved=False).count()
-        )
+        if hasattr(entry, 'unresolved_comment_count'):
+            return entry.unresolved_comment_count
+        return entry.entrycomment_set.filter(parent=None, is_resolved=False).count()
 
     def create(self, validated_data):
         if validated_data.get('project') is None:
