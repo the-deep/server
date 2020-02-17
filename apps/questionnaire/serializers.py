ef11536a27538d0b2a6e3fef945e6cc3d5a142e6
thenav56 <ayernavin@togglecorp.com>
2020-02-17 13:38:50 +0545

    Questionnaire API Changes
    * Add Questionnaire bulk actions
    * Add xlsform to xform util API
    * Add kobo export API
    * Add basic permissions

--- apps/questionnaire/serializers.py
+++ apps/questionnaire/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from drf_dynamic_fields import DynamicFieldsMixin
 from user_resource.serializers import UserResourceSerializer
 
@@ -6,18 +6,10 @@ from deep.serializers import (
     RemoveNullFieldsMixin,
 )
 
-from analysis_framework.serializers import (
-    AnalysisFrameworkSerializer
-)
-
-from project.models import (
-    Project,
-)
-
 from .models import (
-    QuestionBase,
     Questionnaire,
     Question,
+    FrameworkQuestion,
     CrisisType,
 )
 
@@ -31,80 +23,60 @@ class CrisisTypeSerializer(
         model = CrisisType
 
 
-class QuestionSerializer(
-    RemoveNullFieldsMixin,
-    UserResourceSerializer,
-):
-    enumerator_skill_detail = serializers.SerializerMethodField()
-    data_collection_technique_detail = serializers.SerializerMethodField()
-    importance_detail = serializers.SerializerMethodField()
+class QuestionBaseSerializer(RemoveNullFieldsMixin, serializers.ModelSerializer):
+    enumerator_skill_display = serializers.CharField(source='get_enumerator_skill_display', read_only=True)
+    data_collection_technique_display = serializers.CharField(
+        source='get_data_collection_technique_display', read_only=True)
+    importance_display = serializers.CharField(source='get_importance_display', read_only=True)
     crisis_type_detail = CrisisTypeSerializer(source='crisis_type', read_only=True)
 
+
+class QuestionSerializer(QuestionBaseSerializer):
     class Meta:
         model = Question
         fields = '__all__'
 
-    def get_enumerator_skill_detail(self, obj):
-        enumerator_skill_map = dict(obj.ENUMERATOR_SKILL_OPTIONS)
 
-        return {
-            'key': obj.enumerator_skill,
-            'value': enumerator_skill_map.get(obj.enumerator_skill)
-        }
-
-    def get_data_collection_technique_detail(self, obj):
-        data_collection_technique_map = dict(obj.DATA_COLLECTION_TECHNIQUE_OPTIONS)
-
-        return {
-            'key': obj.data_collection_technique,
-            'value': data_collection_technique_map.get(obj.data_collection_technique),
-        }
-
-    def get_importance_detail(self, obj):
-        importance_map = dict(obj.IMPORTANCE_OPTIONS)
-
-        return {
-            'key': obj.importance,
-            'value': importance_map.get(obj.importance)
-        }
+class FrameworkQuestionSerializer(QuestionBaseSerializer):
+    class Meta:
+        model = FrameworkQuestion
+        fields = ('__all__')
 
 
-class QuestionnaireSerializer(
+class MiniQuestionnaireSerializer(
     RemoveNullFieldsMixin,
     DynamicFieldsMixin,
     UserResourceSerializer,
 ):
-    enumerator_skill_detail = serializers.SerializerMethodField()
-    data_collection_technique_detail = serializers.SerializerMethodField()
+    enumerator_skill_display = serializers.CharField(source='get_enumerator_skill_display', read_only=True)
+    data_collection_technique_display = serializers.CharField(
+        source='get_data_collection_technique_display', read_only=True)
 
-    project_framework_detail = AnalysisFrameworkSerializer(
-        source='project.analysis_framework', read_only=True)
-
-    questions = QuestionSerializer(source='question_set',
-                                   many=True,
-                                   required=False)
     crisis_type_detail = CrisisTypeSerializer(source='crisis_type', read_only=True)
+    active_questions_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Questionnaire
-        fields = ('__all__')
+        fields = '__all__'
 
     def create(self, validated_data):
         questionnaire = super().create(validated_data)
         return questionnaire
 
-    def get_enumerator_skill_detail(self, obj):
-        enumerator_skill_map = dict(QuestionBase.ENUMERATOR_SKILL_OPTIONS)
 
-        return {
-            'key': obj.enumerator_skill,
-            'value': enumerator_skill_map.get(obj.enumerator_skill)
-        }
+class QuestionnaireSerializer(MiniQuestionnaireSerializer):
+    questions = QuestionSerializer(source='question_set', many=True, required=False)
 
-    def get_data_collection_technique_detail(self, obj):
-        data_collection_technique_map = dict(QuestionBase.DATA_COLLECTION_TECHNIQUE_OPTIONS)
 
-        return {
-            'key': obj.data_collection_technique,
-            'value': data_collection_technique_map.get(obj.data_collection_technique),
-        }
+class KoboToolboxExportSerializer(serializers.Serializer):
+    file = serializers.FileField()
+    access_code = serializers.CharField(required=False)
+    username = serializers.CharField(required=False)
+    password = serializers.CharField(required=False)
+
+    def validate(self, data):
+        if data.get('access_code') is None and (
+                data.get('username') is None or data.get('password') is None
+        ):
+            raise exceptions.ValidationError('Requires access_code or [username, password]')
+        return data
