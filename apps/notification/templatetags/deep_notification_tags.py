83b03ce32b63add9f44646e01ba0bb4004143650
thenav56 <ayernavin@togglecorp.com>
2020-06-11 11:18:09 +0545

    Add Markdown support for entry comment email

---
+++ apps/notification/templatetags/deep_notification_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+from mdmail.api import EmailContent
+
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def markdown_render(value):
+    content = EmailContent(value)
+    return content.html
