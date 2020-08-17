a6c38191cdb7cc7c63c938940c1eba4e92ab6556
bewakes <bewakepandey@gmail.com>
2020-08-17 13:28:51 +0545

    Update requirements, add similarity check and tests

--- apps/lead/tasks/text_processing.py
+++ apps/lead/tasks/text_processing.py
@@ -1,13 +1,14 @@
 import os
 import hashlib
 from typing import List, Tuple, Optional
-import requests
-# from celery import shared_task
-# import langdetect
+from typing_extensions import TypedDict
+import langdetect
 
-# from deep_utils.deduplication.vector_generator import create_trigram_vector
-# from deep_utils.deduplication.elasticsearch import add_to_index, search_similar
-# from deep_utils.deduplication.utils import es_wrapper
+
+from deep_utils.dynamodb.models import Source
+from deep_utils.deduplication.vector_generator import create_trigram_vector, SUPPORTED_LANGUAGES
+from deep_utils.deduplication.elasticsearch import search_similar
+from deep_utils.deduplication.utils import es_wrapper, remove_puncs_and_extra_spaces
 
 from gallery.models import File
 
@@ -15,32 +16,64 @@ import logging
 logger = logging.getLogger(__file__)
 
 DEDUPLICATION_LAMBDA_ENDPOINT = os.environ.get('DEDUPLICATION_LAMBDA_ENDPOINT')
+SIMILAR_FETCH_COUNT = 20
+SIMILARITY_THRESHOLD = 0.9
 
 ErrorString = Optional[str]
 
 
-def get_duplicate_leads(lead_data: dict) -> Tuple[List[int], ErrorString]:
-    # Create source key from website url or attachment(s3) url which will then be
-    # passed to lambda to check for duplicate_leads
-    source_key = None
+class LeadIdSimilarity(TypedDict):
+    lead_id: str
+    similarity_score: float
+
+
+# NOTE: environment variable 'SOURCE_TABLE_NAME' should be defined, used internally by deep_utils.dynamodb
+def get_source_extract_from_pynamodb(source_key):
+    source = Source.get(source_key)
+    if source is None:
+        return None
+    return source.extract.simplified_text
+
+
+def get_source_key(lead_data: dict) -> Optional[str]:
     attachment = lead_data.get('attachment')
     url = lead_data.get('url')
-    project = lead_data['project']
     if attachment:
         gallery_file = File.objects.filter(id=attachment.get('id')).first()
         if gallery_file is None:
-            return [], None
-
+            return None
         s3url = gallery_file.file.url
-        source_key = f's3::{s3url}'
+        return f's3::{s3url}'
+    if url:
+        return hashlib.sha224(url.encode()).hexdigest()
+    return None
 
-    elif url:
-        source_key = hashlib.sha224(url.encode()).hexdigest()
 
-    url = f'{DEDUPLICATION_LAMBDA_ENDPOINT}?source_key={source_key}&project={project}'
-    response = requests.get(url)
-    if response.status_code != 200:
-        logger.warning(f'Non OK response from deduplication lambda: {response.text}')
-        return [], "Error response from server"
-    results: List[int] = response.json()['results']
-    return results, None
+def get_duplicate_leads(lead_data: dict) -> Tuple[List[LeadIdSimilarity], ErrorString]:
+    source_key = get_source_key(lead_data)
+    project = lead_data['project']
+
+    extract = get_source_extract_from_pynamodb(source_key)
+    if not extract:
+        # TODO: probably raise error telling source is not yet extracted
+        return [], None
+
+    lang = langdetect.detect(extract)
+    if lang not in SUPPORTED_LANGUAGES:
+        return [], None
+
+    es = es_wrapper(DEDUPLICATION_LAMBDA_ENDPOINT)
+    vector = create_trigram_vector(remove_puncs_and_extra_spaces(extract))
+    index_name = f'{project}-{lang}-index'
+
+    similar_response = search_similar(SIMILAR_FETCH_COUNT, dict(vector1=vector), index_name, es)
+    hits = similar_response['hits']
+    if hits['max_score'] < SIMILARITY_THRESHOLD:
+        return [], None
+
+    similar_leads = [
+        dict(lead_id=x['_id'], similarity_score=x['_score'])
+        for x in hits['hits']
+        if x['_score'] > SIMILARITY_THRESHOLD
+    ]
+    return similar_leads, None
