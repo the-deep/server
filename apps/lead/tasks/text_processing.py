f88b1168bccd42c8af3d43d96462c02bc9925d16
bewakes <bewakepandey@gmail.com>
2020-08-24 16:08:51 +0545

    Add deduplication pipeline

--- apps/lead/tasks/text_processing.py
+++ apps/lead/tasks/text_processing.py
@@ -1,23 +1,32 @@
 import os
 import hashlib
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, NewType
 from typing_extensions import TypedDict
 import langdetect
 
 
 from deep_utils.dynamodb.models import Source
 from deep_utils.deduplication.vector_generator import create_trigram_vector, SUPPORTED_LANGUAGES
-from deep_utils.deduplication.elasticsearch import search_similar
+from deep_utils.deduplication.elasticsearch import (
+    search_similar,
+    create_knn_vector_index_if_not_exists,
+    add_to_index,
+    Es,  # This is just the type
+)
 from deep_utils.deduplication.utils import es_wrapper, remove_puncs_and_extra_spaces
 
 from gallery.models import File
 
+
 import logging
 logger = logging.getLogger(__file__)
 
-DEDUPLICATION_LAMBDA_ENDPOINT = os.environ.get('DEDUPLICATION_LAMBDA_ENDPOINT')
+DEDUPLICATION_ES_ENDPOINT = os.environ.get('DEDUPLICATION_ES_ENDPOINT')
+DEDUPLICATION_ES_STAGE = os.environ.get('DEDUPLICATION_ES_STAGE', 'local')
 SIMILAR_FETCH_COUNT = 20
 SIMILARITY_THRESHOLD = 0.9
+VECTOR_SIZE = 10000
+VECTOR_NAME = 'vector1'  # KNN Vector name
 
 ErrorString = Optional[str]
 
@@ -27,8 +36,22 @@ class LeadIdSimilarity(TypedDict):
     similarity_score: float
 
 
+class LeadDuplicationInfo:
+    def __init__(
+            self, vector: List[float] = None, es: Es = None, error: str = None,
+            lang: str = None, similar_leads: List[LeadIdSimilarity] = None, index_name: str = None):
+        self.vector = vector
+        self.es = es
+        self.lang = lang
+        self.similar_leads = similar_leads
+        self.error = error
+        self.index_name = index_name
+
+
 # NOTE: environment variable 'SOURCE_TABLE_NAME' should be defined, used internally by deep_utils.dynamodb
-def get_source_extract_from_pynamodb(source_key):
+def get_source_extract_from_pynamodb(source_key: Optional[str]) -> Optional[str]:
+    if not source_key:
+        return None
     source = Source.get(source_key)
     if source is None:
         return None
@@ -49,31 +72,46 @@ def get_source_key(lead_data: dict) -> Optional[str]:
     return None
 
 
-def get_duplicate_leads(lead_data: dict) -> Tuple[List[LeadIdSimilarity], ErrorString]:
+def get_duplicate_leads(lead_data: dict) -> LeadDuplicationInfo:
     source_key = get_source_key(lead_data)
-    project = lead_data['project']
+    project = lead_data['project'].id
+    return get_duplicate_leads_source_key_project(source_key, project)
 
+
+def get_duplicate_leads_source_key_project(source_key: Optional[str], project: int) -> LeadDuplicationInfo:
     extract = get_source_extract_from_pynamodb(source_key)
+    # TODO: check status of source extraction
     if not extract:
         # TODO: probably raise error telling source is not yet extracted
-        return [], None
+        return LeadDuplicationInfo()
 
     lang = langdetect.detect(extract)
+    es = es_wrapper(DEDUPLICATION_ES_ENDPOINT)
+    lead_duplication_info = LeadDuplicationInfo(es=es, lang=lang)
     if lang not in SUPPORTED_LANGUAGES:
-        return [], None
+        return lead_duplication_info
 
-    es = es_wrapper(DEDUPLICATION_LAMBDA_ENDPOINT)
-    vector = create_trigram_vector(remove_puncs_and_extra_spaces(extract))
-    index_name = f'{project}-{lang}-index'
+    vector = create_trigram_vector(lang, remove_puncs_and_extra_spaces(extract))
+    lead_duplication_info.vector = vector
+    logger.info(DEDUPLICATION_ES_STAGE, project, lang)
+    index_name = f'{DEDUPLICATION_ES_STAGE}-{project}-{lang}-index'
+    lead_duplication_info.index_name = index_name
 
-    similar_response = search_similar(SIMILAR_FETCH_COUNT, dict(vector1=vector), index_name, es)
+    create_knn_vector_index_if_not_exists(index_name, VECTOR_SIZE, es)
+    similar_response = search_similar(SIMILAR_FETCH_COUNT, (VECTOR_NAME, vector), index_name, es)
     hits = similar_response['hits']
-    if hits['max_score'] < SIMILARITY_THRESHOLD:
-        return [], None
+    max_score = hits['max_score'] or 0  # this can be None
+    if max_score < SIMILARITY_THRESHOLD:
+        return lead_duplication_info
 
-    similar_leads = [
+    lead_duplication_info.similar_leads = [
         dict(lead_id=x['_id'], similarity_score=x['_score'])
         for x in hits['hits']
         if x['_score'] > SIMILARITY_THRESHOLD
     ]
-    return similar_leads, None
+    return lead_duplication_info
+
+
+def add_lead_to_index(lead, lead_duplication_info: LeadDuplicationInfo):
+    vector_dict = dict(vector1=lead_duplication_info.vector)
+    add_to_index(lead.id, vector_dict, lead_duplication_info.index_name, lead_duplication_info.es)
