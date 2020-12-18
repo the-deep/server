14b09f543639b5834909e8b56d6cf87161eecd04
thenav56 <ayernavin@togglecorp.com>
2020-12-18 13:41:16 +0545

    EMM writable only on Lead Create

--- deep/serializers.py
+++ deep/serializers.py
@@ -96,3 +96,17 @@ def StringListField():
         default=list,
         allow_empty=True,
     )
+
+
+class WriteOnlyOnCreateSerializerMixin():
+    """
+    Allow to define fields only writable on creation
+    """
+    def get_fields(self, *args, **kwargs):
+        fields = super().get_fields(*args, **kwargs)
+        write_only_on_create_fields = getattr(self.Meta, 'write_only_on_create_fields', [])
+        request = self.context.get('request', None)
+        if request and getattr(request, 'method', None) != 'POST':
+            for field in write_only_on_create_fields:
+                fields[field].read_only = True
+        return fields
