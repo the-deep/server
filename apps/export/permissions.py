e45b9af7b8dc7d7bcf066500d68512a36c487676
Rishi <khatririshi2430@gmail.com>
2020-12-29 08:16:54 +0545

    Add test case for delete export

--- apps/export/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class ExportViewSetPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if (view.action in ('list', 'update', 'destroy') or (request.user and request.user.is_authenticated)):
-            return True
-        return False
