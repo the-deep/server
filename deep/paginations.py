fe2b3fd4658379df307d2199ab9647abaeca2022
Rishi <khatririshi2430@gmail.com>
2021-02-03 14:05:37 +0545

    Add pagination

--- deep/paginations.py
+++ deep/paginations.py
@@ -9,3 +9,8 @@ class AutocompleteSetPagination(pagination.LimitOffsetPagination):
 class SmallSizeSetPagination(pagination.LimitOffsetPagination):
     default_limit = 10
     max_limit = 25
+
+
+class AssignmentPagination(pagination.LimitOffsetPagination):
+    default_limit = 5
+    max_limit = 10
