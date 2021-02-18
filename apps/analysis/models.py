50ccc25676772dae4f1e306a6a0a86e56b3f5368
Rishi <khatririshi2430@gmail.com>
2021-02-18 13:35:26 +0545

    Add new model for analysis

--- apps/analysis/models.py
+++ apps/analysis/models.py
@@ -1,20 +1,42 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
+from user.models import User
 from project.models import Project
 from entry.models import Entry
 
 
 class Analysis(models.Model):
     title = models.CharField(max_length=255)
-    main_statement = models.TextField(blank=True)
-    information_gap = models.TextField(blank=True)
-    project = models.ForeignKey(
-        Project,
+    team_lead = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    created_on = models.DateField(blank=True, null=True)
+    analysis_pillars = models.ManyToManyField(
+        'AnalysisPillar',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class AnalysisPillar(models.Model):
+    main_statement = models.TextField()
+    information_gap = models.TextField()
+    filters = JSONField(blank=True, null=True, default=None)
+    assignee = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
         on_delete=models.CASCADE
     )
 
     def __str__(self):
-        return f'{self.title} - {self.project.title}'
+        return self.main_statement
 
 
 class AnalyticalStatement(models.Model):
@@ -25,8 +47,8 @@ class AnalyticalStatement(models.Model):
         through_fields=('analytical_statement', 'entry'),
         blank=True,
     )
-    analysis = models.ForeignKey(
-        Analysis,
+    analysis_pillar = models.ForeignKey(
+        AnalysisPillar,
         on_delete=models.CASCADE
     )
     include_in_report = models.BooleanField(default=False)
