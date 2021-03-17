59c0dd4ad33bf40cb4434ef32f52323030b79b76
thenav56 <ayernavin@togglecorp.com>
2021-03-17 11:53:23 +0545

    Update email templates

--- apps/quality_assurance/admin.py
+++ apps/quality_assurance/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EntryReviewComment, EntryReviewCommentText
+
+
+class EntryReviewCommentTextInline(admin.StackedInline):
+    model = EntryReviewCommentText
+    extra = 0
+
+
+@admin.register(EntryReviewComment)
+class EntryReviewCommentAdmin(admin.ModelAdmin):
+    inlines = [EntryReviewCommentTextInline]
+    autocomplete_fields = (
+        'created_by', 'mentioned_users', 'entry'
+    )
