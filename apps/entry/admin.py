434f0f7c9a98ed5dda61cf0636574a62da723066
thenav56 <ayernavin@togglecorp.com>
2021-03-17 11:53:23 +0545

    Update email templates

--- apps/entry/admin.py
+++ apps/entry/admin.py
@@ -14,7 +14,6 @@ from entry.models import (
     # Entry Group
     ProjectEntryLabel,
     LeadEntryGroup,
-    EntryGroupLabel,
 )
 
 
@@ -52,6 +51,7 @@ class EntryAdmin(VersionAdmin):
         'lead', 'project', 'created_by', 'created_at',
         query_buttons('View', [inline[0] for inline in custom_inlines]),
     ]
+    search_fields = ('lead__title',)
     list_filter = ('project', 'created_by', 'created_at')
     autocomplete_fields = (
         'lead', 'project', 'created_by', 'modified_by', 'analysis_framework', 'tabular_field',
