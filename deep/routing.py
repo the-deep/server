7176193a35636b18f222c6968f7b834d098967ff
bimal prasad pandey <pandeybi125@gmail.com>
2021-04-13 10:53:16 +0545

    Upgrade django version

--- deep/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import route_class
-from utils.websocket.dummy_consumer import DummyConsumer
-from utils.websocket.subscription import SubscriptionConsumer
-
-
-channel_routing = [
-    route_class(DummyConsumer, path=r'/test/$'),
-    route_class(SubscriptionConsumer, path=r'/subscribe/$'),
-]
--- utils/websocket/dummy_consumer.py
+++
@@ -1,9 +0,0 @@
-from channels.generic.websockets import JsonWebsocketConsumer
-
-
-class DummyConsumer(JsonWebsocketConsumer):
-    def connection_groups(self):
-        return ['dummy']
-
-    def receive(self, content):
-        self.send(content)
