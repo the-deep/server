ae7c1d52d5795ce66bad34562168c54c981450f2
annshress <ann.shress@gmail.com>
2021-05-03 13:32:07 +0545

    Remove lookup_expr for MultiChoiceFilter
    By default it applies `OR` filtering:
    https://django-filter.readthedocs.io/en/stable/ref/filters.html#multiplechoicefilter

--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -132,7 +132,6 @@ class FileFilterSet(UserResourceFilterSet):
     projects = django_filters.ModelMultipleChoiceFilter(
         field_name='projects',
         queryset=Project.objects.all(),
-        lookup_expr='in',
         widget=django_filters.widgets.CSVWidget,
         method=filter_files_by_projects,
     )
