9cffa4c2360ee98dc6fb749cebb1bcd745b698f7
annshress <ann.shress@gmail.com>
2021-05-04 14:33:55 +0545

    Fix project memberships update
    - distinct after difference is not allowed

--- apps/project/receivers.py
+++ apps/project/receivers.py
@@ -22,8 +22,7 @@ def refresh_project_memberships_usergroup_modified(sender, instance, **kwargs):
     existing_members.update(role=instance.role)
 
     project_ug_members = User.objects.filter(usergroup__project=project)
-    new_users = project_ug_members.difference(project.get_all_members()).\
-        distinct()
+    new_users = project_ug_members.exclude(id__in=project.get_all_members()).distinct()
     for user in new_users:
         project.add_member(user, role=instance.role, linked_group=user_group)
 
