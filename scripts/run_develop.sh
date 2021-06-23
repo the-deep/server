1fab1b2a84f8d0d0fb28ed121b9cfa074f700073
thenav56 <ayernavin@togglecorp.com>
2021-06-23 11:33:40 +0545

    Add pipenv
    - Upgraded all the packages.
    - Using python buster image as base image.

--- scripts/run_develop.sh
+++ scripts/run_develop.sh
@@ -1,19 +1,8 @@
 #!/bin/bash -x
 
-export PYTHONUNBUFFERED=1
 export DOCKER_HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
-PYTHON_MYPY_DEP_PATH=/code/.mypy-dep/
-
-pip3 install -r requirements.txt
 
 mkdir -p /var/run/celery/
-
-if [ "${USING_MYPY}" == 'true' ]; then
-    # TODO: better/faster way?
-    rm -rf $PYTHON_MYPY_DEP_PATH/*
-    pip3 install -r requirements.txt -t $PYTHON_MYPY_DEP_PATH
-    rm -rf $PYTHON_MYPY_DEP_PATH/typing_extensions*
-fi
 
 python3 manage.py migrate --no-input
 # python3 manage.py createinitialrevisions
