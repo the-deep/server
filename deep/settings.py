047bad86243d81f65fee578ea426f324aef155d7
thenav56 <ayernavin@togglecorp.com>
2021-06-26 22:08:08 +0545

    Change Analysis pillar summary Endpoint.
    - New endpoint `/api/v1/projects/<int>/analysis/<int>/pillars/summary/`
    - Add deep base project permission to Analysis
    - Add ModifyPermission to all Analysis viewsets
    - Disable django-debug-toolbar logging (conflicts with current logging)
    - Update test cases.

--- deep/settings.py
+++ deep/settings.py
@@ -623,6 +623,22 @@ DEBUG_TOOLBAR_CONFIG = {
         'debug_toolbar.panels.templates.TemplatesPanel',
     ],
 }
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    # ENABLING THIS WILL REMOVE LOGS FROM stdout "debug_toolbar.panels.logging.LoggingPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+]
+
 if DEBUG and 'DOCKER_HOST_IP' in os.environ:
     INSTALLED_APPS += ['debug_toolbar']
     MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
