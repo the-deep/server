ec98ad6e860785125a2611d985d9c80e09665d18
thenav56 <ayernavin@togglecorp.com>
2021-07-05 21:04:03 +0545

    Add login/logout Query/Mutation
    - Add factory-boy.
    - Refactor captcha logic. (Add Tests)
    - Add test for login/logout Query/Mutation.

--- apps/analysis_framework/mutation.py
+++ apps/analysis_framework/mutation.py
@@ -1,6 +1,4 @@
 import graphene
-from django.contrib.auth.models import User
-from django.contrib.auth import login
 from rest_framework import permissions
 
 from analysis_framework.models import AnalysisFramework
@@ -57,21 +55,7 @@ class DeleteAnalysisFramework(DeleteMutation):
     permission_classes = [permissions.IsAuthenticated, ModifyPermission]
 
 
-class Login(graphene.Mutation):
-    class Arguments:
-        email = graphene.String(required=True)
-
-    ok = graphene.Boolean()
-
-    @staticmethod
-    def mutate(root, info, email):
-        user = User.objects.get(email=email)
-        login(info.context.request, user)
-        return Login(ok=True)
-
-
 class Mutation(object):
     create_analysis_framework = CreateAnalysisFramework.Field()
     update_analysis_framework = UpdateAnalysisFramework.Field()
     delete_analysis_framework = DeleteAnalysisFramework.Field()
-    login = Login.Field()
--- deep/schema.py
+++ deep/schema.py
@@ -1,10 +1,12 @@
 import graphene
 
 from analysis_framework import mutation as af_mutations, schema as af_schema
+from user import mutation as user_mutations, schema as user_schema
 
 
 class Query(
     af_schema.Query,
+    user_schema.Query,
     graphene.ObjectType
 ):
     pass
@@ -12,6 +14,7 @@ class Query(
 
 class Mutation(
     af_mutations.Mutation,
+    user_mutations.Mutation,
     graphene.ObjectType
 ):
     pass
