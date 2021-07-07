3ba6747d6193961ff5505b153c118fbbd584fb47
k9845 <khatririshi2430@gmail.com>
2021-07-07 13:36:08 +0545

    Add start_date and end_date in analysis summary api

--- apps/analysis/serializers.py
+++ apps/analysis/serializers.py
@@ -176,6 +176,7 @@ class AnalysisSummarySerializer(serializers.ModelSerializer):
         fields = (
             'id', 'title', 'team_lead', 'team_lead_details',
             'publication_date', 'pillars',
+            'start_date', 'end_date',
             'analyzed_entries', 'analyzed_sources', 'total_entries',
             'total_sources', 'created_at', 'modified_at',
         )
