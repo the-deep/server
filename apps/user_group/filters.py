50712fa3bd8456868c56e88857465d372b496415
thenav56 <ayernavin@togglecorp.com>
2021-07-09 16:36:20 +0545

    Add mutation/query for user-groups.
    - TODO: Add test
    - Upgrade to graphql 3 (using beta graphene release)

---
+++ apps/user_group/filters.py
@@ -0,0 +1,17 @@
+import django_filters
+
+from .models import UserGroup
+
+
+class UserGroupFilterSet(django_filters.FilterSet):
+    is_current_user_member = django_filters.BooleanFilter(
+        field_name='is_current_user_member', method='filter_with_membership')
+
+    class Meta:
+        model = UserGroup
+        fields = ['id']
+
+    def filter_with_membership(self, queryset, name, value):
+        if value is not None:
+            queryset = queryset.filter(is_current_user_member=value)
+        return queryset
