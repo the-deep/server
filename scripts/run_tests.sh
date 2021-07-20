9084bc8737280fe152e216a07cb29b2cd08f7bac
thenav56 <ayernavin@togglecorp.com>
2021-07-20 18:56:46 +0545

    Replace travis-ci with github-action.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -x
 
 export PYTHONUNBUFFERED=1
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$CI" == "true" ]; then
-    pip3 install -r requirements.txt
-    pip3 install codecov pytest-xdist
-    python3 manage.py migrate --no-input
-    python3 manage.py createinitialrevisions
-    python3 manage.py run_celery_dev &
+    pip3 install coverage pytest-xdist
 
     set -e
+    # Wait until database is ready
+    $BASE_DIR/wait-for-it.sh ${DATABASE_HOST:-db}:${DATABASE_PORT-5432}
+
     echo 'import coverage; coverage.process_startup()' > /code/sitecustomize.py
     COVERAGE_PROCESS_START=`pwd`/.coveragerc COVERAGE_FILE=`pwd`/.coverage PYTHONPATH=`pwd` py.test -n auto --dist=loadfile --durations=10
+
+    # Collect/Generate reports
     coverage combine
     coverage report -i
     coverage html -i
-    codecov
+    coverage xml
     set +e
 else
     py.test
