adbea32af91d415d53fe7da1bc8ef308ceb74d66
thenav56 <ayernavin@togglecorp.com>
2021-07-21 13:50:38 +0545

    Add lead mutation

--- deep/schema.py
+++ deep/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django.debug import DjangoDebug
 from django.conf import settings
 
-from project import schema as pj_schema
+from project import schema as pj_schema, mutation as pj_mutation
 from analysis_framework import mutation as af_mutation, schema as af_schema
 from user import mutation as user_mutation, schema as user_schema
 from user_group import mutation as user_group_mutation, schema as user_group_schema
@@ -23,6 +23,7 @@ class Mutation(
     af_mutation.Mutation,
     user_mutation.Mutation,
     user_group_mutation.Mutation,
+    pj_mutation.Mutation,
     graphene.ObjectType
 ):
     pass
