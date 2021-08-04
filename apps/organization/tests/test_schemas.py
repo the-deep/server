c7725f5f019b59c460b5829a843a8c02c1014601
k9845 <khatririshi2430@gmail.com>
2021-08-04 14:28:05 +0545

    Add schema for lead-options in project

---
+++ apps/organization/tests/test_schemas.py
@@ -0,0 +1,32 @@
+from utils.graphene.tests import GraphQLTestCase
+
+from organization.factories import OrganizationTypeFactory
+from organization.models import OrganizationType
+from user.factories import UserFactory
+
+
+class TestOrganizationTypeQuery(GraphQLTestCase):
+    def test_lead_query(self):
+        query = '''
+            query OrganizationType {
+                organizationTypes {
+                    results {
+                        id
+                        title
+                        description
+                        shortName
+                    }
+                    totalCount
+                }
+            }
+        '''
+        OrganizationType.objects.all().delete()
+        OrganizationTypeFactory.create_batch(3)
+        user = UserFactory.create()
+
+        # Without authentication -----
+        self.query_check(query, assert_for_error=True)
+
+        self.force_login(user)
+        content = self.query_check(query, assert_for_error=False)
+        self.assertEqual(len(content['data']['organizationTypes']['results']), 3, content)
