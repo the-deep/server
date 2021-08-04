a559365708001702d7bbbf6415853111a996c4ec
k9845 <khatririshi2430@gmail.com>
2021-08-04 14:28:05 +0545

    Add schema for lead-options in project

--- apps/lead/factories.py
+++ apps/lead/factories.py
@@ -4,7 +4,12 @@ from factory.django import DjangoModelFactory
 
 from project.factories import ProjectFactory
 from gallery.factories import FileFactory
-from .models import Lead, EMMEntity
+from .models import (
+    Lead,
+    EMMEntity,
+    LeadGroup,
+    LeadEMMTrigger
+)
 
 
 class LeadFactory(DjangoModelFactory):
@@ -24,9 +29,34 @@ class LeadFactory(DjangoModelFactory):
             for author in extracted:
                 self.authors.add(author)
 
+    @factory.post_generation
+    def emm_entities(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for emm_entity in extracted:
+                self.emm_entities.add(emm_entity)
+
 
 class EmmEntityFactory(DjangoModelFactory):
     name = factory.Sequence(lambda n: f'emm-name-{n}')
 
     class Meta:
         model = EMMEntity
+
+
+class LeadGroupFactory(DjangoModelFactory):
+    title = factory.Sequence(lambda n: f'LeadGroup-{n}')
+    project = factory.SubFactory(ProjectFactory)
+
+    class Meta:
+        model = LeadGroup
+
+
+class LeadEMMTriggerFactory(DjangoModelFactory):
+    lead = factory.SubFactory(LeadFactory)
+    emm_keyword = factory.Sequence(lambda n: f'emm_keyword-{n}')
+    emm_risk_factor = factory.Sequence(lambda n: f'emm_risk_factor-{n}')
+
+    class Meta:
+        model = LeadEMMTrigger
