bbfbcab229ad2b8f52e84eeb226e4930316023fe
thenav56 <ayernavin@togglecorp.com>
2021-09-16 10:33:11 +0545

    Clean up obsolute AF filters/exportables on AF update.

--- apps/analysis_framework/utils.py
+++ apps/analysis_framework/utils.py
@@ -9,20 +9,34 @@ def update_widget(widget):
     if widget_module is None:
         raise Exception(f'Unknown widget type: {widget.widet_id}')
 
+    new_filter_keys = []
     if hasattr(widget_module, 'get_filters'):
         filters = widget_module.get_filters(widget, widget_properties) or []
         for filter in filters:
+            filter_key = filter.get('key', widget.key)
+            new_filter_keys.append(filter_key)
             filter['title'] = filter.get('title', widget.title)
             Filter.objects.update_or_create(
                 analysis_framework=widget.analysis_framework,
                 widget_key=widget.key,
-                key=filter.get('key', widget.key),
+                key=filter_key,
                 defaults=filter,
             )
 
+    # Remove obsolute filters
+    Filter.objects.filter(
+        # Only look at current widget key + AF
+        analysis_framework=widget.analysis_framework,
+        widget_key=widget.key,
+    ).exclude(
+        key__in=new_filter_keys,
+    ).delete()
+
+    new_exportable_keys = []
     if hasattr(widget_module, 'get_exportable'):
         exportable = widget_module.get_exportable(widget, widget_properties)
         if exportable:
+            new_exportable_keys.append(widget.key)
             Exportable.objects.update_or_create(
                 analysis_framework=widget.analysis_framework,
                 widget_key=widget.key,
@@ -30,6 +44,14 @@ def update_widget(widget):
                     'data': exportable,
                 },
             )
+
+    # Remove obsolute Exportables
+    Exportable.objects.filter(
+        # Only look at current AF
+        analysis_framework=widget.analysis_framework,
+    ).exclude(
+        widget_key__in=new_exportable_keys,
+    ).delete()
 
 
 def update_widgets(widget_id=None, **widget_filters):
