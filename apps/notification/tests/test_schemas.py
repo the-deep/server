7adcf35a9634242e32cf04aeac367f5cee2ff1a5
thenav56 <ayernavin@togglecorp.com>
2021-11-09 12:00:41 +0545

    Add test for Notification Filters
    - Use Datetime for timestamp filters

--- apps/notification/tests/test_schemas.py
+++ apps/notification/tests/test_schemas.py
@@ -1,5 +1,7 @@
-from utils.graphene.tests import GraphQLTestCase
+import datetime
+import pytz
 
+from utils.graphene.tests import GraphQLTestCase
 from user.factories import UserFactory
 from project.factories import ProjectFactory
 
@@ -103,4 +105,84 @@ class TestNotificationQuerySchema(GraphQLTestCase):
         content = _query_check(other_notification)
         self.assertEqual(content['data']['notification'], None, content)
 
-    # TODO: Add test for filter
+    def test_notifications_with_filter_query(self):
+        query = '''
+            query MyQuery (
+                $timestamp: DateTime,
+                $timestampLte: DateTime,
+                $timestampGte: DateTime,
+                $status: NotificationStatusEnum,
+                $notificationType: NotificationTypeEnum,
+                $isPending: Boolean,
+            ) {
+              notifications (
+                timestamp: $timestamp,
+                timestampLte: $timestampLte,
+                timestampGte: $timestampGte,
+                status: $status,
+                notificationType: $notificationType,
+                isPending: $isPending,
+              ) {
+                totalCount
+                results {
+                  id
+                  status
+                  project {
+                    id
+                    title
+                  }
+                  notificationType
+                  timestamp
+                  notificationTypeDisplay
+                  statusDisplay
+                  data
+                }
+              }
+            }
+        '''
+
+        project = ProjectFactory.create()
+        user = UserFactory.create()
+        notification_meta = dict(
+            project=project,
+            receiver=user,
+        )
+        NotificationFactory.create(
+            notification_type=Notification.Type.PROJECT_JOIN_REQUEST,
+            status=Notification.Status.UNSEEN,
+            timestamp=datetime.datetime(2021, 1, 1, 0, 0, 0, 0, tzinfo=pytz.UTC),
+            **notification_meta,
+        )
+        NotificationFactory.create(
+            notification_type=Notification.Type.PROJECT_JOIN_REQUEST,
+            status=Notification.Status.SEEN,
+            timestamp=datetime.datetime(2021, 1, 10, 0, 0, 0, 0, tzinfo=pytz.UTC),
+            **notification_meta,
+        )
+        NotificationFactory.create(
+            notification_type=Notification.Type.PROJECT_JOIN_REQUEST_ABORT,
+            status=Notification.Status.UNSEEN,
+            timestamp=datetime.datetime(2021, 2, 1, 0, 0, 0, 0, tzinfo=pytz.UTC),
+            data={'status': 'pending'},
+            **notification_meta,
+        )
+
+        def _query_check(filters, **kwargs):
+            return self.query_check(query, variables=filters, **kwargs)
+
+        # --- With login
+        self.force_login(user)
+        for filters, count in [
+                ({'status': self.genum(Notification.Status.SEEN)}, 1),
+                ({'status': self.genum(Notification.Status.UNSEEN)}, 2),
+                ({'notificationType': self.genum(Notification.Type.PROJECT_JOIN_REQUEST)}, 2),
+                ({'notificationType': self.genum(Notification.Type.PROJECT_JOIN_REQUEST_ABORT)}, 1),
+                ({'isPending': True}, 1),
+                ({'isPending': False}, 2),
+                ({'timestampGte': '2021-01-01T00:00:00+00:00', 'timestampLte': '2021-01-01T00:00:00+00:00'}, 1),
+                ({'timestampGte': '2021-01-01T00:00:00+00:00', 'timestampLte': '2021-02-01T00:00:00+00:00'}, 3),
+                ({'timestamp': '2021-01-01T00:00:00+00:00'}, 1),
+        ]:
+            content = _query_check(filters)
+            self.assertEqual(content['data']['notifications']['totalCount'], count, f'\n{filters=} \n{content=}')
+            self.assertEqual(len(content['data']['notifications']['results']), count, f'\n{filters=} \n{content=}')
