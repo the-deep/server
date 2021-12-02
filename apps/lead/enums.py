160f8a666ba34ab11f64e87fdeb7bbba0e954c83
thenav56 <ayernavin@togglecorp.com>
2021-12-02 14:19:13 +0545

    Add ignore logger to sentry.
    - Add a prototype ordering enum for leads.
    - Fix tests (Remove conditional widgets).
    - Update schema.graphql

--- apps/lead/enums.py
+++ apps/lead/enums.py
@@ -1,3 +1,5 @@
+import graphene
+
 from utils.graphene.enums import (
     convert_enum_to_graphene_enum,
     get_enum_name_from_django_field,
@@ -19,3 +21,27 @@ enum_map = {
         (Lead.source_type, LeadSourceTypeEnum),
     )
 }
+
+
+# TODO: Define this dynamically through a list?
+class LeadOrderingEnum(graphene.Enum):
+    # ASC
+    ASC_ID = 'id'
+    ASC_CREATED_AT = 'created_at'
+    ASC_TITLE = 'title'
+    ASC_SOURCE = 'source'
+    ASC_PUBLISHED_ON = 'published_on'
+    ASC_CREATED_BY = 'created_by'
+    ASC_ASSIGNEE = 'assignee'
+    ASC_PRIORITY = 'priority'
+    ASC_PAGE_COUNT = 'leadpreview__page_count'
+    # DESC
+    DESC_ID = '-id'
+    DESC_CREATED_AT = '-created_at'
+    DESC_TITLE = '-title'
+    DESC_SOURCE = '-source'
+    DESC_PUBLISHED_ON = '-published_on'
+    DESC_CREATED_BY = '-created_by'
+    DESC_ASSIGNEE = '-assignee'
+    DESC_PRIORITY = '-priority'
+    DESC_PAGE_COUNT = 'leadpreview__page_count'
