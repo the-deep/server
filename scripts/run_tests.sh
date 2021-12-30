4ea644e42c0c48f7e37faa36afeda54f516b3dec
thenav56 <ayernavin@togglecorp.com>
2021-12-30 14:16:31 +0545

    Using wait-from-it from package.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,13 +2,14 @@
 
 export PYTHONUNBUFFERED=1
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$(dirname "$BASE_DIR")
 
 if [ "$CI" == "true" ]; then
     pip3 install coverage pytest-xdist
 
     set -e
     # Wait until database is ready
-    $BASE_DIR/wait-for-it.sh ${DATABASE_HOST:-db}:${DATABASE_PORT-5432}
+    wait-for-it ${DATABASE_HOST:-db}:${DATABASE_PORT-5432}
 
     # To show migration logs
     ./manage.py test -v 2 deep.tests.test_fake
@@ -16,12 +17,18 @@ if [ "$CI" == "true" ]; then
     # Run all tests now
     echo 'import coverage; coverage.process_startup()' > /code/sitecustomize.py
     COVERAGE_PROCESS_START=`pwd`/.coveragerc COVERAGE_FILE=`pwd`/.coverage PYTHONPATH=`pwd` py.test -n auto --reuse-db --dist=loadfile --durations=10
+    rm /code/sitecustomize.py
 
     # Collect/Generate reports
     coverage combine
     coverage report -i
     coverage html -i
     coverage xml
+
+    mkdir -p $ROOT_DIR/coverage/
+    mv htmlcov $ROOT_DIR/coverage/
+    mv coverage.xml $ROOT_DIR/coverage/
+
     set +e
 else
     py.test
