1529fce31c9c0da7252bd3877eca689d732c8c95
thenav56 <ayernavin@togglecorp.com>
2022-01-12 00:53:57 +0545

    Email static fix.

--- apps/notification/templatetags/deep_notification_tags.py
+++ apps/notification/templatetags/deep_notification_tags.py
@@ -1,9 +1,14 @@
 from django import template
+from django.conf import settings
+from django.templatetags.static import static
+from django.core.files.storage import FileSystemStorage, get_storage_class
 
 from mdmail.api import EmailContent
 
 
 register = template.Library()
+
+StorageClass = get_storage_class()
 
 
 @register.filter(is_safe=True)
@@ -12,3 +17,12 @@ def markdown_render(value):
         content = EmailContent(value)
         return content.html
     return '-'
+
+
+@register.filter(is_safe=True)
+def static_full_path(path):
+    static_path = static(path)
+    if StorageClass == FileSystemStorage:
+        return f"{settings.HTTP_PROTOCOL}://{settings.DJANGO_API_HOST}{static_path}"
+    # With s3 storage
+    return static_path
