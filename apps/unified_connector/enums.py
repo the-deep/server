04a0444e5c56822a86c6b86eee250197a0e269e1
thenav56 <ayernavin@gmail.com>
2022-02-24 16:05:01 +0545

    Add query for Unified Connector

---
+++ apps/unified_connector/enums.py
@@ -0,0 +1,52 @@
+import graphene
+
+from utils.graphene.enums import (
+    convert_enum_to_graphene_enum,
+    get_enum_name_from_django_field,
+)
+
+from .models import ConnectorSource
+
+ConnectorSourceEnum = convert_enum_to_graphene_enum(ConnectorSource.Source, name='ConnectorSourceEnum')
+
+enum_map = {
+    get_enum_name_from_django_field(field): enum
+    for field, enum in (
+        (ConnectorSource.source, ConnectorSourceEnum),
+    )
+}
+
+
+class UnifiedConnectorOrderingEnum(graphene.Enum):
+    # ASC
+    ASC_ID = 'id'
+    ASC_CREATED_AT = 'created_at'
+    ASC_TITLE = 'title'
+    # DESC
+    DESC_ID = f'-{ASC_ID}'
+    DESC_CREATED_AT = f'-{ASC_CREATED_AT}'
+    DESC_TITLE = f'-{ASC_TITLE}'
+
+
+class ConnectorSourceOrderingEnum(graphene.Enum):
+    # ASC
+    ASC_ID = 'id'
+    ASC_CREATED_AT = 'created_at'
+    ASC_TITLE = 'title'
+    ASC_SOURCE = 'source'
+    # DESC
+    DESC_ID = f'-{ASC_ID}'
+    DESC_CREATED_AT = f'-{ASC_CREATED_AT}'
+    DESC_TITLE = f'-{ASC_TITLE}'
+    DESC_SOURCE = f'-{ASC_SOURCE}'
+
+
+class ConnectorSourceLeadOrderingEnum(graphene.Enum):
+    # ASC
+    ASC_ID = 'id'
+    ASC_LEAD_CREATED_AT = 'connector_lead__created_at'
+    ASC_LEAD_TITLE = 'connector_lead__title'
+    # DESC
+    DESC_ID = f'-{ASC_ID}'
+    DESC_LEAD_CREATED_AT = f'-{ASC_LEAD_CREATED_AT}'
+    DESC_LEAD_TITLE = f'-{ASC_LEAD_TITLE}'
--- deep/filter_set.py
+++ deep/filter_set.py
@@ -1,5 +1,6 @@
 import django_filters
 from django import forms
+from django.db import models
 
 
 class DjangoFilterCSVWidget(django_filters.widgets.CSVWidget):
@@ -14,3 +15,13 @@ class DjangoFilterCSVWidget(django_filters.widgets.CSVWidget):
                 return value
             return [x.strip() for x in value.strip().split(',') if x.strip()]
         return None
+
+
+class OrderEnumMixin():
+    def ordering_filter(self, qs, _, value):
+        for ordering in value:
+            if ordering.startswith('-'):
+                qs = qs.order_by(models.F(ordering).desc(nulls_last=True))
+            else:
+                qs = qs.order_by(models.F(ordering).asc(nulls_last=True))
+        return qs
