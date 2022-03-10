4576be6a6f81e92c55aa3340f93eb69164b63caf
thenav56 <ayernavin@gmail.com>
2022-03-10 11:14:21 +0545

    Fix user resource base class issue.

--- apps/user_resource/serializers.py
+++ apps/user_resource/serializers.py
@@ -23,12 +23,15 @@ class UserResourceBaseSerializer(serializers.Serializer):
     version_id = serializers.SerializerMethodField()
 
     def create(self, validated_data):
-        validated_data['created_by'] = self.context['request'].user
-        validated_data['modified_by'] = self.context['request'].user
+        if 'created_by' in self.Meta.model._meta._forward_fields_map:
+            validated_data['created_by'] = self.context['request'].user
+        if 'modified_by' in self.Meta.model._meta._forward_fields_map:
+            validated_data['modified_by'] = self.context['request'].user
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        validated_data['modified_by'] = self.context['request'].user
+        if 'modified_by' in self.Meta.model._meta._forward_fields_map:
+            validated_data['modified_by'] = self.context['request'].user
         return super().update(instance, validated_data)
 
     def get_version_id(self, resource):
