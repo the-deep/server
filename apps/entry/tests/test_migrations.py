b20f0441362c7b83418b4e5d377578572445db39
thenav56 <ayernavin@gmail.com>
2022-03-28 15:57:26 +0545

    Add excerpt_modified flag.
    - Migrate data to dropped_excerpt for old entries.

--- apps/entry/tests/test_migrations.py
+++ apps/entry/tests/test_migrations.py
@@ -1,9 +1,16 @@
 import importlib
 
+from django.db import models
 from deep.tests import TestCase
 
 from entry.models import Entry
 from quality_assurance.models import EntryReviewComment
+
+from project.factories import ProjectFactory
+from analysis_framework.factories import AnalysisFrameworkFactory
+from user.factories import UserFactory
+from lead.factories import LeadFactory
+from entry.factories import EntryFactory
 
 
 class TestCustomMigrationsLogic(TestCase):
@@ -16,30 +23,19 @@ class TestCustomMigrationsLogic(TestCase):
     def test_test_entry_review_verify_control_migrations(self):
         migration_file = importlib.import_module('entry.migrations.0031_entry-migrate-verify-to-review-comment')
 
-        user1 = self.create_user(
-            username='jon1@dave.com',
-            email='jon1@dave.com',
-            password='test',
-        )
-        user2 = self.create_user(
-            username='jon2@dave.com',
-            email='jon2@dave.com',
-            password='test',
-        )
-        user3 = self.create_user(
-            username='jon3@dave.com',
-            email='jon3@dave.com',
-            password='test',
-        )
-        # Additional non-active user
-        self.create_user(username='jon4@dave.com', email='jon3@dave.com', password='test')
+        # 3 normal users + Additional non-active user
+        user1, user2, user3, _ = UserFactory.create_batch(4)
+
+        af = AnalysisFrameworkFactory.create()
+        project = ProjectFactory.create(analysis_framework=af)
+        lead = LeadFactory.create(project=project)
 
         # Create entry before data migrate
-        self.create_entry(controlled=True, controlled_changed_by=user1)
-        self.create_entry(controlled=True, controlled_changed_by=user2)
-        self.create_entry(controlled=False, controlled_changed_by=user3)
-        self.create_entry(controlled=True, controlled_changed_by=None)
-        self.create_entry(controlled=False, controlled_changed_by=None)
+        EntryFactory.create(lead=lead, controlled=True, controlled_changed_by=user1)
+        EntryFactory.create(lead=lead, controlled=True, controlled_changed_by=user2)
+        EntryFactory.create(lead=lead, controlled=False, controlled_changed_by=user3)
+        EntryFactory.create(lead=lead, controlled=True, controlled_changed_by=None)
+        EntryFactory.create(lead=lead, controlled=False, controlled_changed_by=None)
 
         # Apply the migration logic
         migration_file.migrate_entry_controlled_to_review_comment_(Entry, EntryReviewComment)
@@ -64,3 +60,38 @@ class TestCustomMigrationsLogic(TestCase):
         # All controlled, controlled_changed_by should be reset.
         assert Entry.objects.filter(controlled=True).count() == 0
         assert Entry.objects.filter(controlled_changed_by__isnull=False).count() == 0
+
+    def test_entry_dropped_excerpt_migrations(self):
+        def _get_excerpt_snapshot():
+            return list(Entry.objects.order_by('id').values_list('excerpt', 'dropped_excerpt', 'excerpt_modified'))
+
+        migration_file = importlib.import_module('entry.migrations.0036_entry_excerpt_modified')
+
+        af = AnalysisFrameworkFactory.create()
+        project = ProjectFactory.create(analysis_framework=af)
+        lead = LeadFactory.create(project=project)
+
+        # Create entry before data migrate
+        EntryFactory.create(lead=lead, excerpt='', dropped_excerpt='')
+        EntryFactory.create(lead=lead, excerpt='sample-1', dropped_excerpt='')
+        EntryFactory.create(lead=lead, excerpt='sample-2', dropped_excerpt='sample-2-updated')
+        EntryFactory.create(lead=lead, excerpt='sample-3', dropped_excerpt='sample-3')
+        old_excerpt_snaphost = _get_excerpt_snapshot()
+
+        # Apply the migration logic
+        migration_file._update_entry_excerpt(Entry)
+        new_excerpt_snaphost = _get_excerpt_snapshot()
+
+        assert Entry.objects.count() == 4
+        assert Entry.objects.filter(dropped_excerpt='').count() == 1
+        assert Entry.objects.filter(excerpt_modified=True).count() == 1
+        assert Entry.objects.filter(dropped_excerpt=models.F('excerpt')).count() == 3
+        assert Entry.objects.exclude(dropped_excerpt=models.F('excerpt')).count() == 1
+
+        assert new_excerpt_snaphost != old_excerpt_snaphost
+        assert new_excerpt_snaphost == [
+            ('', '', False),
+            ('sample-1', 'sample-1', False),
+            ('sample-2', 'sample-2-updated', True),
+            ('sample-3', 'sample-3', False),
+        ]
