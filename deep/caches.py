00a8f5aff3895ec20216b2a3b20f23fd4b3a0de9
thenav56 <ayernavin@gmail.com>
2023-01-14 23:08:01 +0545

    Add cache to top ten nodes in ExploreDeep
    - Update typing

--- deep/caches.py
+++ deep/caches.py
@@ -1,4 +1,9 @@
-from django.core.cache import caches
+import json
+import hashlib
+from typing import Union
+
+from django.core.cache import cache, caches
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 local_cache = caches['local-memory']
@@ -27,3 +32,53 @@ class CacheKey:
         TOTAL_AUTHORS = _PREFIX + 'TOTAL-AUTHORS'
         TOTAL_PUBLISHERS = _PREFIX + 'TOTAL-PUBLISHERS'
         TOP_TEN_AUTHORS = _PREFIX + 'TOP-TEN-AUTHORS'
+        TOP_TEN_PUBLISHERS = _PREFIX + 'TOP-TEN-PUBLISHERS'
+        TOP_TEN_FRAMEWORKS = _PREFIX + 'TOP-TEN-FRAMEWORKS'
+        TOP_TEN_PROJECTS_BY_USERS = _PREFIX + 'TOP-TEN-PROJECTS-BY-USERS'
+        TOP_TEN_PROJECTS_BY_ENTRIES = _PREFIX + 'TOP-TEN-PROJECTS-BY-ENTRIES'
+
+
+class CacheHelper:
+    @staticmethod
+    def calculate_md5_str(string):
+        hash_md5 = hashlib.md5()
+        hash_md5.update(string)
+        return hash_md5.hexdigest()
+
+    @classmethod
+    def generate_hash(cls, item: Union[None, str, dict]) -> str:
+        if item is None:
+            return ''
+        hashable = None
+        if type(item) == str:
+            hashable = item
+        elif type(item) == dict:
+            hashable = json.dumps(
+                item,
+                sort_keys=True,
+                indent=2,
+                cls=DjangoJSONEncoder,
+            ).encode('utf-8')
+        else:
+            raise Exception(f'Unknown Type: {type(item)}')
+        return cls.calculate_md5_str(hashable)
+
+    @staticmethod
+    def gql_cache(cache_key, cache_key_gen=None, timeout=60):
+        def _dec(func):
+            def _caller(*args, **kwargs):
+                if cache_key_gen:
+                    _cache_key = cache_key.format(
+                        cache_key_gen(*args, **kwargs)
+                    )
+                else:
+                    _cache_key = cache_key
+                return cache.get_or_set(
+                    _cache_key,
+                    lambda: func(*args, **kwargs),
+                    timeout,
+                )
+            _caller.__name__ = func.__name__
+            _caller.__module__ = func.__module__
+            return _caller
+        return _dec
