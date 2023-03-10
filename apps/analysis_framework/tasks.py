24fb2321f4c8b34f30d158e1ff1f47ede33ed2b1
thenav56 <ayernavin@gmail.com>
2023-03-10 10:40:52 +0545

    Fix encoding issue with boto3 s3 upload

--- apps/analysis_framework/tasks.py
+++ apps/analysis_framework/tasks.py
@@ -2,11 +2,12 @@ import logging
 
 from celery import shared_task
 from django.utils import timezone
+from django.core.files.base import ContentFile
 
 from utils.common import redis_lock, get_temp_file
 
 from .models import AnalysisFramework
-from .export import export_af_to_csv
+from analysis_framework.export import export_af_to_csv
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +20,8 @@ def export_af_to_csv_task(af_id):
         with get_temp_file(suffix='.csv', mode='w+') as file:
             export_af_to_csv(af, file)
             time_str = timezone.now().strftime('%Y-%m-%d%z')
-            af.export.save(f'AF_Export_{af.id}_{time_str}.csv', file)
+            file.seek(0)
+            af.export.save(f'AF_Export_{af.id}_{time_str}.csv', ContentFile(file.read().encode('utf-8')))
     except Exception:
         logger.error(f'Failed to export AF: {af_id}', exc_info=True)
         return False
