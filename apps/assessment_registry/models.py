3479bf5f74937d7c66f8f039dc6a87f4715ab9e9
rup <rup.rajbanshi@togglecorp.com>
2023-04-28 17:09:10 +0545

    Add models for Additional Document and Focus

--- apps/assessment_registry/models.py
+++ apps/assessment_registry/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-from apps.user_resource.models import UserResource
-from apps.geo.models import Region
-from apps.organization.models import Organization
-# Create your models here.
+from django.contrib.postgres.fields import ArrayField
+
+from user_resource.models import UserResource
+from geo.models import Region
+from organization.models import Organization
+from gallery.models import File
 
 
-class Assessment_Registry(UserResource):
-    class CrisisType(models.IntegerChoice):
+class AssessmentRegistry(UserResource):
+    class CrisisType(models.IntegerChoices):  # NOTE: Here the values are selected as per the pk value in deep data to make it easier during data migration
         EARTH_QUAKE = 1, 'Earth Quake'
         GROUND_SHAKING = 2, 'Ground Shaking'
         TSUNAMI = 3, 'Tsunami'
@@ -17,7 +19,7 @@ class Assessment_Registry(UserResource):
         AVALANCE = 8, 'Avalance'
         LANDSLIDE = 9, 'Landslide'
         SUBSIDENCE = 10, 'Subsidence'
-        EXTRA_TROPICAL_CYCLONE = 11, 'Extra Tropical Cyclone'
+        # EXTRA_TROPICAL_CYCLONE = 11, 'Extra Tropical Cyclone'
         TROPICAL_CYCLONE = 12, 'Tropical Cyclone'
         LOCAL_STROM = 13, 'Local/Convective Strom'
         EXTRA_TROPICAL_CYCLONE = 14, 'Extra Tropical Cyclone'
@@ -35,27 +37,27 @@ class Assessment_Registry(UserResource):
         POPULATION_DISPLACEMENT = 112, 'Population Displacement'
         CONFLICT = 79, 'Conflict'
 
-    class PreparednessType(models.IntegerChoice):
+    class PreparednessType(models.IntegerChoices):
         WITH_PREPAREDNESS = 26, 'With Preparedness'
         WITHOUT_PREPAREDNESS = 27, 'Without Preparedness'
 
-    class ExternalSupportType(models.IntegerChoice):
+    class ExternalSupportType(models.IntegerChoices):
         EXTERNAL_SUPPORT_RECIEVED = 29, 'External Support Received'
         NO_EXTERNAL_SUPPORT_RECEIVED = 30, 'No External Support Received'
 
-    class CoordinationType(models.IntegerChoice):
+    class CoordinationType(models.IntegerChoices):
         COORDINATED = 37, 'Coordinated Joint'
         HARMONIZED = 38, 'Coordinated Harmonized'
         UNCOORDINATED = 39, 'Uncoordinated'
 
-    class Type(models.IntegerChoice):
+    class Type(models.IntegerChoices):
         INITIAL = 67, 'Initial'
         RAPID = 65, 'Rapid'
         IN_DEPTH = 64, 'In depth'
         MONITORING = 68, 'Monitoring'
         OTHER = 72, 'Other'
 
-    class FamilyType(models.IntegerChoice):
+    class FamilyType(models.IntegerChoices):
         DISPLACEMENT_TRAKING_MATRIX = 74, 'Displacement Traking Matrix'
         MULTI_CLUSTER_INITIAL_AND_RAPID_ASSESSMENT = 75, 'Multi Cluster Initial and Rapid Assessment (MIRA)'
         MULTI_SECTORIAL_NEEDS_ASSESSMENT = 76, 'Multi sectorial Needs Assessment (MSNA)'
@@ -73,23 +75,35 @@ class Assessment_Registry(UserResource):
         SMART_NUTRITION_SURVEY = 214, 'Smart Nutrition Survey'
         OTHER = 277, 'Other'
 
-    class FrequencyType(models.IntegerChoice):
+    class FrequencyType(models.IntegerChoices):
         ONE_OFF = 56, 'One off'
         REGULAR = 57, 'Regular'
 
-    class ConfidentialityType(models.IntegerField):
+    class ConfidentialityType(models.IntegerChoices):
         UNPROTECTED = 58, 'Unprotected'
         CONFIDENTIAL = 73, 'Confidential'
 
-    class Language(models.IntegerChoice):
+    class Language(models.IntegerChoices):
         ENGLISH = 61, 'English'
         FRENCH = 62, 'French'
         SPANISH = 63, 'Spanish'
         PORTUGESE = 146, 'Portugese'
         ARABIC = 66, 'Arabic'
 
-    # Metadata
-    # Metadata
+    class FocusType(models.IntegerChoices):
+        CONTEXT = 74, 'Context'
+        SHOCK_EVENT = 75, 'Shock/Event'
+        DISPLACEMENT = 42, 'Displacement'
+        HUMANITERIAN_ACCESS = 8, 'Humaniterian Access'
+        INFORMATION_AND_COMMUNICATION = 41, 'Information and Communication'
+        IMPACT = 9, 'Impact (Scope and Scale)'
+        HUMANITARIAN_CONDITIONS = 10, 'Humanitarian Conditions'
+        RESPONSE_AND_CAPACITIES = 11, 'Response and Capacities'
+        CURRENT_AND_FORECASTED_PRIORITIES = 76, 'Current and Forecasted Priorities'
+        COVID_19_CONTAINMENT_MEASURES = 107, 'Covid 19 Conntainment Measures'
+
+    # Metadata Group
+    # Background Fields
     bg_countries = models.ManyToManyField(Region)
     bg_crisis_type = models.IntegerField(choices=CrisisType.choices)
     bg_crisis_start_date = models.DateField()
@@ -103,7 +117,7 @@ class Assessment_Registry(UserResource):
     family = models.IntegerField(choices=FamilyType.choices)
     frequency = models.IntegerField(choices=FrequencyType.choices)
     confidentiality = models.IntegerField(choices=ConfidentialityType.choices)
-    language = models.ArrayField(choices=Language.choices)
+    language = ArrayField(models.IntegerField(choices=Language.choices))
     no_of_pages = models.IntegerField(null=True, blank=True)
 
     # Dates
@@ -112,8 +126,40 @@ class Assessment_Registry(UserResource):
     publishcation_date = models.DateField(null=True, blank=True)
 
     # Stakeholders
-    lead_organization = models.ManyToManyField(Organization)
-    international_partners = models.ManyToManyField(Organization)
-    donor = models.ManyToManyField(Organization)
-    national_partners = models.ManyToManyField(Organization)
-    government = models.ManyToManyField(Organization)
+    lead_organization = models.ManyToManyField(Organization, related_name='lead_org_assessment_reg')
+    international_partners = models.ManyToManyField(Organization, related_name='int_partners_assessment_reg')
+    donor = models.ManyToManyField(Organization, related_name='donor_assessment_reg')
+    national_partners = models.ManyToManyField(Organization, related_name='national_partner_assessment_reg')
+    government = models.ManyToManyField(Organization, related_name='gov_assessment_reg')
+
+    # Focus
+    # Focus Sectors
+    # focuses = models.IntegerField(choices=FocusType.choices)
+    # sectors = models.IntegerField(choices=SectorType.choices)
+    # protection_info_mgmt = models.IntegerField(choices=ProtectionInfoType.choices)
+    # affected_group = models.IntegerField(choices=AffectedGroupType.choices)
+    location = models.ManyToManyField(Region, related_name='focus_location_assessment_reg')
+
+
+class AdditionalDocument(UserResource):
+    class DocumentType(models.IntegerChoices):
+        EXECUTIVE_SUMMARY = 1000, 'Executive summary'
+        ASSESSMENT_DATABASE = 1001, 'Assessment database'
+        QUESTIONAIRE = 1002, 'Questionaire'
+        MISCELLANESOUS = 1003, 'Miscellaneous'
+
+    assessment_registry = models.ForeignKey(
+        AssessmentRegistry,
+        on_delete=models.CASCADE,
+        related_name='assessment_reg_add_document',
+    )
+    document_type = models.IntegerField(choices=DocumentType.choices)
+    file = models.ForeignKey(
+        File,
+        on_delete=models.SET_NULL,
+        related_name='assessment_reg_file',
+        null=True, blank=True
+    )
+    external_link = models.TextField(blank=True)
+
+
