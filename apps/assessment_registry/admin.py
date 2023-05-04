5189a619ff14331dd3d60328ab4ca2f961d706b0
rup <rup.rajbanshi@togglecorp.com>
2023-05-04 17:09:33 +0545

    Add focus, sectors, affected_groups, location for Focus tab

--- apps/assessment_registry/admin.py
+++ apps/assessment_registry/admin.py
@@ -1,3 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import AssessmentRegistry
 
 # Register your models here.
+
+
+@admin.register(AssessmentRegistry)
+class AssessmentRegistryAdmin(admin.ModelAdmin):
+    list_display = ('id',)
