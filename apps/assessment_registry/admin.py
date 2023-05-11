d60646a8a47eac93c2b5a7e146e16dbfe21e93c9
rup <rup.rajbanshi@togglecorp.com>
2023-05-11 16:32:32 +0545

    Add mutation for Methodology tab

--- apps/assessment_registry/admin.py
+++ apps/assessment_registry/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import AssessmentRegistry
+from .models import AssessmentRegistry, MethodologyAttribute
 
 # Register your models here.
 
 
 @admin.register(AssessmentRegistry)
 class AssessmentRegistryAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+
+@admin.register(MethodologyAttribute)
+class MethodlogyAttributeAdmin(admin.ModelAdmin):
     list_display = ('id',)
