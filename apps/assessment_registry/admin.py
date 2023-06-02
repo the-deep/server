132995c4bd27ebcce006b19279c27f067e4b23e9
rup <rup.rajbanshi@togglecorp.com>
2023-06-02 16:54:04 +0545

    Add mutation for summary tab
    - Add query to list questions

--- apps/assessment_registry/admin.py
+++ apps/assessment_registry/admin.py
@@ -4,10 +4,29 @@ from .models import (
     AssessmentRegistry,
     MethodologyAttribute,
     Summary,
+    Question,
+    Answer,
 )
 
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sector', 'question')
+    readonly_fields = ('created_by', 'modified_by', 'client_id',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        else:
+            obj.modified_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question')
+
+
 class MethodologyAttributeInline(admin.TabularInline):
     model = MethodologyAttribute
     extra = 1
