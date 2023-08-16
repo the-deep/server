f8cd62ad110a91d90cb9c3d928adac0763afcbec
Rup-Narayan-Rajbanshi <rup.rajbanshi@togglecorp.com>
2023-08-16 15:14:00 +0545

    Add dataloader for SummarySubPillarIssue

--- apps/assessment_registry/dataloaders.py
+++ apps/assessment_registry/dataloaders.py
@@ -5,7 +5,10 @@ from promise import Promise
 from django.utils.functional import cached_property
 from utils.graphene.dataloaders import DataLoaderWithContext, WithContextMixin
 
-from .models import AssessmentRegistryOrganization
+from .models import (
+    AssessmentRegistryOrganization,
+    SummarySubPillarIssue,
+)
 
 
 class AssessmentRegistryOrganizationsLoader(DataLoaderWithContext):
@@ -17,8 +20,21 @@ class AssessmentRegistryOrganizationsLoader(DataLoaderWithContext):
         return Promise.resolve([_map.get(key) for key in keys])
 
 
+class AssessmentRegistryIssueLoader(DataLoaderWithContext):
+    def batch_load_fn(self, keys):
+        qs = SummarySubPillarIssue.objects.filter(assessment_registry__in=keys)
+        _map = defaultdict(list)
+        for issue in qs.all():
+            _map[issue.assessment_registry_id].append(issue)
+        return Promise.resolve([_map.get(key) for key in keys])
+
+
 class DataLoaders(WithContextMixin):
 
     @cached_property
     def stakeholders(self):
         return AssessmentRegistryOrganizationsLoader(context=self.context)
+
+    @cached_property
+    def issues(self):
+        return AssessmentRegistryIssueLoader(context=self.context)
