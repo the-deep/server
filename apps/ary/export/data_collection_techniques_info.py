ec3f73364ab1809b91480a2de90199f8bdc7638d
Rup-Narayan-Rajbanshi <rup.rajbanshi@togglecorp.com>
2023-09-13 15:46:07 +0545

    Error fixed for affecred group, display enums

--- apps/ary/export/data_collection_techniques_info.py
+++ apps/ary/export/data_collection_techniques_info.py
@@ -18,12 +18,12 @@ def get_data_collection_techniques_info(assessment):
     )
     data = [
         {
-            "Data Collection Technique":attr.get_data_collection_technique_display(),
-            "Sampling Size":attr.get_sampling_approach_display(),
-            "Sampling Approach":attr.sampling_size,
-            "Proximity":attr.get_proximity_display(),
-            "Unit of Analysis":attr.get_unit_of_analysis_display(),
-            "Unit of reporting":attr.get_unit_of_reporting_display(),
+            "Data Collection Technique": attr.get_data_collection_technique_display(),
+            "Sampling Size": attr.get_sampling_approach_display(),
+            "Sampling Approach": attr.sampling_size,
+            "Proximity": attr.get_proximity_display(),
+            "Unit of Analysis": attr.get_unit_of_analysis_display(),
+            "Unit of reporting": attr.get_unit_of_reporting_display(),
         }for attr in attributes
     ]
     return {
--- apps/ary/export/stakeholders_info.py
+++ apps/ary/export/stakeholders_info.py
@@ -1,20 +1,18 @@
-from ary.models import MetadataField
+from assessment_registry.models import AssessmentRegistryOrganization
 
-
-STAKEHOLDERS_SOURCE_TYPES = [MetadataField.ORGANIZATIONS]
 
 default_values = {
     'stakeholders': None,
 }
 
-from assessment_registry.models import AssessmentRegistryOrganization
+
 def get_stakeholders_info(assessment):
-    stakeholders_info=[
+    stakeholders_info = [
         {
             'name': org.organization.title,
             'type': org.get_organization_type_display()
         }for org in AssessmentRegistryOrganization.objects.filter(assessment_registry=assessment)
-    ] #TODO : Add Dataloaders
+    ]  # TODO : Add Dataloaders
 
     return {
         'stakeholders': stakeholders_info,
