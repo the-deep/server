e5f8cc90148139a85acdcfa016c46dcc4ac4f46b
Robert Koikai <robert@datafriendlyspace.org>
2023-11-06 13:11:57 +0545

    Add recently Used filter in AnalysisFramework

--- apps/analysis_framework/filter_set.py
+++ apps/analysis_framework/filter_set.py
@@ -8,9 +8,13 @@ from user_resource.filters import (
 from .models import (
     AnalysisFramework,
 )
+from entry.models import Entry
+from django.utils import timezone
+from datetime import timedelta
 
 
 class AnalysisFrameworkFilterSet(UserResourceFilterSet):
+
     class Meta:
         model = AnalysisFramework
         fields = ('id', 'title', 'description', 'created_at',)
@@ -24,12 +28,17 @@ class AnalysisFrameworkFilterSet(UserResourceFilterSet):
             },
         }
 
-
 # ----------------------------- Graphql Filters ---------------------------------------
+
+
 class AnalysisFrameworkGqFilterSet(UserResourceGqlFilterSet):
     search = django_filters.CharFilter(method='search_filter')
     is_current_user_member = django_filters.BooleanFilter(
         field_name='is_current_user_member', method='filter_with_membership')
+    recently_used = django_filters.BooleanFilter(
+        method='filter_recently_used',
+        label='Recently Used',
+    )
 
     class Meta:
         model = AnalysisFramework
@@ -49,4 +58,12 @@ class AnalysisFrameworkGqFilterSet(UserResourceGqlFilterSet):
             else:
                 afs = afs.exclude(filter_query)
             return queryset.filter(id__in=afs)
+        return queryset
+
+    def filter_recently_used(self, queryset, name, value):
+        if value:
+            # Calculate the date for "recent" usage (e.g., within the last 6 months or 180 days)
+            recent_usage_cutoff = timezone.now() - timedelta(days=180)
+            entries_qs = Entry.objects.filter(modified_at__gte=recent_usage_cutoff)
+            return queryset.filter(id__in=entries_qs.values('analysis_framework'))
         return queryset
