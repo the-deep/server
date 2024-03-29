a4e38d968260cfd43be7b728a27c4158d452b755
Aditya Khatri <adityakhatri47@gmail.com>
2024-03-29 11:00:51 +0545

    Add ignore ids filter in draft entries query

--- apps/assisted_tagging/filters.py
+++ apps/assisted_tagging/filters.py
@@ -1,7 +1,11 @@
 import django_filters
 
 from .models import DraftEntry
-from utils.graphene.filters import IDFilter, MultipleInputFilter
+from utils.graphene.filters import (
+    IDFilter,
+    MultipleInputFilter,
+    IDListFilter,
+)
 from .enums import (
     DraftEntryTypeEnum
 )
@@ -10,8 +14,14 @@ from .enums import (
 class DraftEntryFilterSet(django_filters.FilterSet):
     lead = IDFilter(field_name='lead')
     draft_entry_types = MultipleInputFilter(DraftEntryTypeEnum, field_name='type')
+    ignore_ids = IDListFilter(method='filter_ignore_draft_ids', help_text='Ids are filtered out.')
     is_discarded = django_filters.BooleanFilter()
 
     class Meta:
         model = DraftEntry
         fields = ()
+
+    def filter_ignore_draft_ids(self, qs, _, value):
+        if value is None:
+            return qs
+        return qs.exclude(id__in=value)
