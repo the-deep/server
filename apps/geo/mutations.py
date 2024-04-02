59dc6a6a01f1ca9495e3296fae87c633f61c7853
sudan45 <sudan1bhandari@gmail.com>
2024-04-02 16:48:44 +0545

    implemented region in graphql

--- apps/geo/mutations.py
+++ apps/geo/mutations.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import graphene
 
 from geo.models import Region, AdminLevel
@@ -90,3 +91,31 @@ class Mutation():
     create_admin_level = CreateAdminLevel.Field()
     update_admin_level = UpdateAdminLevel.Field()
     publish_region = PublishRegion.Field()
+||||||| parent of d61e37aa0 (implemented region in graphql)
+=======
+import graphene
+from geo.models import Region
+from geo.schema import RegionType
+from geo.serializers import RegionGqSerializer
+
+from deep.permissions import ProjectPermissions as PP
+from utils.graphene.mutation import PsGrapheneMutation, generate_input_type_for_serializer
+
+RegionInputType = generate_input_type_for_serializer(
+    'RegionInputType',
+    serializer_class=RegionGqSerializer,
+)
+
+
+class CreateRegion(PsGrapheneMutation):
+    class Arguments:
+        data = RegionInputType(required=True)
+    model = Region
+    serializer_class = RegionGqSerializer
+    result = graphene.Field(RegionType)
+    permissions = [PP.Permission.UPDATE_PROJECT]
+
+
+class Mutation():
+    create_region = CreateRegion.Field()
+>>>>>>> d61e37aa0 (implemented region in graphql)
--- deep/schema.py
+++ deep/schema.py
@@ -18,6 +18,8 @@ from analysis import public_schema as analysis_public_schema
 from user import mutation as user_mutation, schema as user_schema
 from user_group import mutation as user_group_mutation, schema as user_group_schema
 from organization import schema as organization_schema, mutation as organization_mutation
+from geo import schema as geo_schema
+from organization import schema as organization_schema
 from geo import schema as geo_schema, mutations as geo_mutation
 from notification import schema as notification_schema, mutation as notification_mutation
 from assisted_tagging import schema as assisted_tagging_schema
@@ -73,6 +75,7 @@ class Mutation(
     gallery_mutation.Mutation,
     assessment_registry_mutation.Mutation,
     organization_mutation.Mutation,
+    geo_mutation.Mutation,
     # --
     graphene.ObjectType
 ):
